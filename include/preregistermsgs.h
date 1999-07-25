3aad5003f6ea57fc466e050fcd6f3a39b716f52d
TD10216 <a@b>
1999-07-24 18:50:31 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 13902]

---
+++ include/preregistermsgs.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 1999 by Cobe BCT, Inc.  All rights reserved.
+ *
+ * $Header: Q:/home1/COMMON_PROJECT/Source/INCLUDE/rcs/PreRegisterMsgs.h 1.1 1999/07/24 20:50:41 TD10216 Exp TD10216 $
+ * $Log: PreRegisterMsgs.h $
+ *
+ *
+ * ABSTRACT:Code shared between tcp_gate and everest_logger which
+ * doesnt really belong in any of our libraries.
+ *
+ *
+*/
+
+// SPECIFICATION:	Read File to get pre-registered Everest messages
+//					Parameter: none
+//
+// ERROR HANDLING:	none
+// currently hard-coded for Trima!!
+#define EVERESTMAP "/trima/tcpip/everest_logger.msgs"
+static void PreRegisterMsgs( int RegisterRegistrationMsg )
+{
+	TCPGate_Reg_ReqMsg aRegMsg;
+	FILE *EnumDefFile;
+
+	if((EnumDefFile = fopen(EVERESTMAP,"r")) == NULL)
+	{
+        _FATAL_ERROR( __FILE__,__LINE__, TRACE_GATEWAY, errno,  
+            "Cant open EverstMsgMap file");
+		return;
+	}
+	if(RegisterRegistrationMsg)
+	{
+		aRegMsg.Msg_Struct.Reg_Struct.count = 1;
+		// catch and log all external tcpgate msgs which are registered
+		aRegMsg.Msg_Struct.Reg_Struct.MsgIds[0] = TcpgateRegistration;
+	}
+	else
+		aRegMsg.Msg_Struct.Reg_Struct.count = 0;
+
+	char buf[80];
+	while(fgets(buf,sizeof(buf),EnumDefFile) != NULL)
+	{
+		// only register MAXREG messages at a time
+		if(aRegMsg.Msg_Struct.Reg_Struct.count == MAXREG - 1)
+		{
+			MsgRegistrar.RegisterMessages( &aRegMsg, RegisterMsgs::DontReply);
+			aRegMsg.Msg_Struct.Reg_Struct.count = 0;
+		}
+		char *p = strchr(buf,'\n');
+		if(p)
+			*p = '\0';
+		int b = atoi(buf);
+		aRegMsg.Msg_Struct.Reg_Struct.MsgIds[aRegMsg.Msg_Struct.Reg_Struct.count++] 
+			= (short)b;
+	}
+	fclose(EnumDefFile);
+	// catch remainder or all if < MAXREG - 1
+	if(aRegMsg.Msg_Struct.Reg_Struct.count)	
+		MsgRegistrar.RegisterMessages( &aRegMsg, RegisterMsgs::DontReply);
+}

06e2b8c834d7899e660be273ae556da5bb2b6fd1
BS04481 <a@b>
1999-08-31 15:45:41 -0800

    Create everest log file with sync to disk bits set. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 13926]

--- include/an2_log_agent.hpp
+++ include/an2_log_agent.hpp
@@ -3,6 +3,8 @@
 *
 * $Header: Q:/home1/COMMON_PROJECT/Source/INCLUDE/rcs/AN2_LOG_AGENT.HPP 1.2 1999/08/10 21:57:55 TD10216 Exp BS04481 $
 * $Log: AN2_LOG_AGENT.HPP $
+* Revision 1.2  1999/08/10 21:57:55  TD10216
+* IT3724
 * Revision 1.1  1999/08/04 22:36:23  TD10216
 * Initial revision
 * an2msgs.h
@@ -29,7 +31,7 @@ class an2_log_agent
 		DIR *dirfile;
 		struct dirent *entry;
 		sem_t *LogSem;
-		FILE *fo;
+		int fo;
 		ConfigDataMsgStruct LogStruct;
 		char eString[256];
 
@@ -39,7 +41,7 @@ class an2_log_agent
 						 char const *Value);
 
 	public:
-		an2_log_agent(FILE *, sem_t *s = NULL);
+		an2_log_agent(int, sem_t *s = NULL);
 		~an2_log_agent();
 		int LogAgentData(void);
 };

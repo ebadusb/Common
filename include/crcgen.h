cb75ea9199af152c5a1531a16ed8caf16d0261f6
bd10648 <a@b>
2000-06-07 20:04:21 -0800

    Moved CRC seed into this header file from softcrc.h [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 13981]

--- include/crcgen.h
+++ include/crcgen.h
@@ -6,6 +6,8 @@
  * CHANGELOG:
  *  $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/crcgen.h 1.4 2001/08/22 14:33:20 rm70006 Exp ms10234 $
  *  $Log: crcgen.h $
+ *  Revision 1.1  1999/05/24 23:26:24  TD10216
+ *  Initial revision
  *  Revision 1.3  1998/08/18 23:00:39  TD07711
  *    make header file c++ compatible
  *  8/18/98 - dyes - make header file c++ portable
@@ -31,7 +33,12 @@ extern "C" {
 //    *pcrc is updated with new crc value
 //    returns 0 if success
 //    returns -1 if invalid argument (NULL ptr or length < 0)
+
+
 int crcgen32(unsigned long* pcrc, char* pdata, long length);
+
+// common seed for all users to get common results.
+#define INITCRC_DEFAULT 0
 
 
 #ifdef __cplusplus

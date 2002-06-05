afbbd801e781b15e7d16eb482f99a50a3664e366
Lindner, John <John.Lindner@Terumobct.com>
2002-06-04 18:22:53 -0800

    - added stubs for default level related functions [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14088]

--- datalog/datalog.cpp
+++ datalog/datalog.cpp
@@ -3,6 +3,8 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/datalog/rcs/datalog.cpp 1.13 2003/12/05 16:33:05Z jl11312 Exp rm70006 $
  * $Log: datalog.cpp $
+ * Revision 1.2  2002/05/17 18:28:34  jl11312
+ * - temporary changes for debug use under vxWorks
  * Revision 1.1  2002/05/17 14:51:29  jl11312
  * Initial revision
  *
@@ -41,3 +43,30 @@ DataLog_Critical::~DataLog_Critical()
 {
 }
 
+DataLog_Result datalog_CreateLevel(const char * levelName, DataLog_Handle * handle)
+{
+	handle = NULL;
+	return DataLog_OK;
+}
+
+DataLog_Result	datalog_SetDefaultLevel(DataLog_Handle handle)
+{
+	return DataLog_OK;
+}
+
+DataLog_Result datalog_Print(DataLog_Handle handle, const char * file, int line, const char * format, ...)
+{
+	cout << file << " " << line << " " << format << endl;
+	return DataLog_OK;
+}
+
+DataLog_Result datalog_PrintToDefault(const char * file, int line, const char * format, ...)
+{
+	cout << file << " " << line << " " << format << endl;
+	return DataLog_OK;
+}
+
+ostream & DataLog_DefaultLevel::getStream(const char * fileName, int lineNumber)
+{
+	return cout;
+}

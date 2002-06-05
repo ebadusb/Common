e080173493e7fc1cd7ad2e2ba7118c3c6b1d6dc1
Lindner, John <John.Lindner@Terumobct.com>
2002-06-04 18:24:26 -0800

    - added protection for multiple includes [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14090]

--- include/rtc.h
+++ include/rtc.h
@@ -1,15 +1,20 @@
 /*
  * Copyright (C) 2002 Gambro BCT, Inc.  All rights reserved.
  *
- *	vxWorks utilities for accessing the real-time clock.  This file should be
- * included as part of the build for an vxWorks OS image and is not intended
- * to be compiled separately.
+ *	vxWorks utilities for accessing the real-time clock.
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/rtc.h 1.4 2002/10/08 15:35:17Z jl11312 Exp ms10234 $
  * $Log: rtc.h $
+ * Revision 1.1  2002/05/30 13:00:30  jl11312
+ * Initial revision
  *
  */
 
+#ifndef _RTC_INCLUDE
+#define _RTC_INCLUDE
+
 STATUS setCurrentTimeFromRTC(void);
 STATUS setRTCFromCurrentTime(void);
+
+#endif /* ifndef _RTC_INCLUDE */
 

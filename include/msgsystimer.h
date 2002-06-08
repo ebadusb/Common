ec9884d66264726aab4f8a49a67243fab8f6196b
Scott, Mark <Mark.Scott@terumobct.com>
2002-06-07 16:59:34 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14091]

---
+++ include/msgsystimer.h
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2002 Gambro BCT, Inc.  All rights reserved.
+ *
+ * TITLE:      msgsystimer.h, Message System Timer 
+ *
+ * ABSTRACT:   These classes support message system timer messages. The message
+ *             system timer class interfaces with the message system and the 
+ *             hardware clock to manage the timers.   Only one timer task should 
+ *             created per processor.   
+ */
+
+#ifndef _MSGSYSTIMER_H_
+#define _MSGSYSTIMER_H_
+
+#include <fcntl.h>
+#include <map>
+#include <mqueue.h>
+#include <queue>
+
+#include "messagepacket.h"
+
+class MsgSysTimer
+{
+public:
+   //
+   // Number of retries for all system operations before we consider the
+   //  failure an error
+   static const unsigned int MAX_NUM_RETRIES;
+   //
+   // Time delay between retries ...
+   static const struct timespec RETRY_DELAY;
+
+public:
+
+   //
+   // This function will act as the main entry point for the message system timer task.  
+   //  This function will create the message system timer object.  When this function 
+   //  exits, the message system timer task will also be considered finished.
+   static int MsgSysTimer_main();
+
+   //
+   // Function to get the global message system timer task ...
+   static WIND_TCB *globalMsgSysTimerTid();
+   static MsgSysTimer *globalMsgSysTimer();
+
+   //
+   // Function called whenever any task gets created ...
+   static int taskCreateHook( WIND_TCB *pTcb );
+   //
+   // Function called whenever any task gets deleted ...
+   static int taskDeleteHook( WIND_TCB *pTcb );
+
+private:
+
+   static WIND_TCB    *_TheTimerTid;
+   static MsgSysTimer *_TheTimer;
+
+public:
+
+   MsgSysTimer();
+   virtual ~MsgSysTimer();
+
+   //
+   // Function which will create the message queue, and set up all the 
+   //  necessary structures to start the processing.  
+   bool init();
+
+   //
+   // This function will continue processing indefinitely while it is blocked on the
+   //  timer semaphore.
+   void maintainTimers();
+   void stopLoop() { _StopLoop=true; }
+  
+   //
+   // Dump the contents of this class
+   void dump( ostream &outs );
+
+protected:
+
+   //
+   // Receive a message packet and determine its function 
+   void processMessage( const MessagePacket &mp );
+   
+   //
+   // Function to update the current tick count
+   void updateTime( const unsigned long long tickCount );
+
+   //
+   // Function to register a new timer
+   void registerTimer( const MessagePacket &mp, const unsigned long interval );
+
+   //
+   // Function to deregister the requested timer message
+   void deregisterTimer( const unsigned long mId );
+   void deregisterTimersOfTask( const unsigned long tId );
+
+   //
+   // Function to check all timers to see if they have expired
+   void checkTimers();
+
+   //
+   // Shutdown the message queues ...
+   void shutdown();
+
+   //
+   // Cleanup the priority queue and the map ... 
+   void cleanup();
+
+private:
+
+   unsigned long long _TickCount;
+
+   struct MapEntry;
+   class QueueEntry
+   {
+   public:
+      QueueEntry();
+      QueueEntry( const QueueEntry &qe );
+
+      virtual ~QueueEntry();
+
+      QueueEntry &operator=( const QueueEntry &qe );
+
+      int operator==( const QueueEntry &qe ) const;
+      int operator==( const unsigned long long l ) const;
+      int operator<( const QueueEntry &qe ) const;
+      int operator<( const unsigned long long l ) const;
+      int operator<=( const unsigned long long l ) const;
+      int operator>( const QueueEntry &qe ) const;
+      int operator>( const unsigned long long l ) const;
+      int operator>=( const unsigned long long l ) const;
+
+   public:
+
+      unsigned long long _ExpirationTickCount;
+      MapEntry           *_MapEntryPtr;
+
+   };
+
+   struct MapEntry
+   {
+      MessagePacket *_TimerMessage;
+      unsigned long _Interval;
+   };
+
+   map< unsigned long, MapEntry* > _TimerMsgMap;
+   priority_queue< QueueEntry, vector< QueueEntry >, greater< QueueEntry > > _TimerQueue;
+
+   mqd_t _TimerMQ;
+   mqd_t _RouterMQ;
+
+   bool _StopLoop;
+
+};
+
+#endif
---
+++ router/msgsystimer.cpp
@@ -0,0 +1,468 @@
+/*
+ * Copyright (c) 2002 Gambro BCT, Inc.  All rights reserved.
+ *
+ * TITLE:      msgsystimer.cpp, Message System Timer 
+ *
+ */
+
+#include <stdio.h>
+#include <vxWorks.h>
+#include <taskHookLib.h>
+
+#include "error.h"
+#include "msgsystimer.h"
+
+const unsigned int MsgSysTimer::MAX_NUM_RETRIES=1;
+const struct timespec MsgSysTimer::RETRY_DELAY={ 1 /* seconds */, 0 /*nanoseconds*/ };
+
+WIND_TCB    *MsgSysTimer::_TheTimerTid=0;
+MsgSysTimer *MsgSysTimer::_TheTimer=0;
+
+
+int MsgSysTimer::MsgSysTimer_main()
+{
+   if ( _TheTimer )
+      return !OK;
+
+   MsgSysTimer msgSysTimer;
+   msgSysTimer.init();
+
+   msgSysTimer.maintainTimers();
+
+   return OK;
+}
+
+int MsgSysTimer::taskDeleteHook( WIND_TCB *pTcb )
+{
+   if ( _TheTimerTid == pTcb )
+   {
+      delete _TheTimer; 
+      _TheTimer = 0;
+      _TheTimerTid = 0;
+   }
+
+   if ( _TheTimer )
+   {
+      _TheTimer->deregisterTimersOfTask( (unsigned long) pTcb );
+   }
+   
+   return 1;
+}
+
+MsgSysTimer::MsgSysTimer() 
+ : _TickCount( 0 ),
+   _TimerMsgMap(),
+   _TimerQueue(),
+   _TimerMQ( (mqd_t)ERROR ),
+   _RouterMQ( (mqd_t)ERROR ),
+   _StopLoop( false )
+{
+}
+
+MsgSysTimer::~MsgSysTimer()
+{
+   shutdown();
+   cleanup();
+}
+
+
+bool MsgSysTimer::init()
+{
+   if ( _TheTimer )
+      return false;
+
+   //
+   // Add the task delete hooks to catch all task deletion and 
+   //  keep the timer lists up to date ...
+   taskDeleteHookAdd( (FUNCPTR) &MsgSysTimer::taskDeleteHook );
+
+   //
+   // Set up my queue ...
+   struct mq_attr attr;                                      // message queue attributes 
+   attr.mq_maxmsg = 15; 
+   attr.mq_msgsize = sizeof( MessagePacket );                // set message size 
+   
+   //
+   // Open the message queue for read-only ...
+   unsigned int retries=0;
+   do
+   {
+      _TimerMQ = mq_open( "timertask", O_RDONLY | O_CREAT, 0666, &attr);
+      nanosleep( &MsgSysTimer::RETRY_DELAY, 0 );
+   }
+   while (    _TimerMQ == (mqd_t)ERROR 
+           && retries++ < MAX_NUM_RETRIES );
+
+   if ( _TimerMQ == (mqd_t)ERROR )
+   {
+      //
+      // Error ...
+      _FATAL_ERROR( __FILE__, __LINE__, "Timer message queue open failed" );
+      return false;
+   }
+
+   //
+   // Open the router's queue ...
+   retries=0;
+   do
+   {
+      _RouterMQ = mq_open( "router", O_WRONLY );
+      nanosleep( &MsgSysTimer::RETRY_DELAY, 0 );
+   } 
+   while (    _RouterMQ == (mqd_t)ERROR 
+           && retries++ < MAX_NUM_RETRIES );
+
+   if ( _RouterMQ == (mqd_t)ERROR )
+   {
+      //
+      // Error ...
+      _FATAL_ERROR( __FILE__, __LINE__, "Router message queue open failed" );
+      return false;
+   }
+
+
+   //
+   // Initialize the ISR routine ...
+//    extraAuxClockInit(  10 /* tick rate to notify me every 10ms */ );
+
+
+   //
+   // Set the static pointer to ensure we only init once ...
+   _TheTimer = this;
+   _TheTimerTid = taskTcb( 0 );
+
+   return true;
+}
+
+void MsgSysTimer::maintainTimers()
+{
+   if ( _TimerMQ == 0 )
+      return;
+
+   int size=0;
+   char buffer[ sizeof( MessagePacket ) ];
+   do
+   {
+      //
+      // Read the queue entry ...
+      unsigned int retries=0;
+      while (    ( size = mq_receive( _TimerMQ, &buffer, sizeof( MessagePacket ), 0 ) ) == ERROR 
+              && retries++ < MAX_NUM_RETRIES ) nanosleep( &MsgSysTimer::RETRY_DELAY, 0 );
+      if ( size == ERROR )
+      {
+         //
+         // Error ...
+         _FATAL_ERROR( __FILE__, __LINE__, "Maintain timer =%lx - message queue receive failed" );
+      }
+
+      //
+      // Format the data ...
+      MessagePacket mp;
+      memmove( &mp, &buffer, sizeof( MessagePacket ) );
+
+      if ( mp.validCRC() == false )
+      {
+         //
+         // Error ...
+         char buffer[256];
+         unsigned long crc = mp.crc();
+         mp.updateCRC();
+         sprintf( buffer,"Maintain timer - message CRC validation failed for MsgId=%lx, CRC=%lx and should be %lx",
+                  mp.msgData().msgId(), crc, mp.crc() );
+         _FATAL_ERROR( __FILE__, __LINE__, buffer );
+      }  
+
+      processMessage( mp );
+
+   } while ( _StopLoop == false );
+
+}
+
+void MsgSysTimer::dump( ostream &outs )
+{
+}
+
+void MsgSysTimer::processMessage( const MessagePacket &mp )
+{
+   //
+   // Determine what type of message this is ...
+   //
+   unsigned long long tc;
+   unsigned long interval;
+   switch ( mp.msgData().osCode() )
+   {
+   case MessageData::TIME_UPDATE:
+      memmove( (char *) &tc , 
+               (char *) mp.msgData().msg(), 
+               sizeof( unsigned long long ) );
+      updateTime( tc );
+      break;
+   case MessageData::MESSAGE_MULTICAST:
+   case MessageData::MESSAGE_MULTICAST_LOCAL:
+   case MessageData::SPOOFED_MESSAGE:
+      memmove( (char *) &interval , 
+               (char *) mp.msgData().msg(), 
+               sizeof( unsigned long ) );
+      if ( interval == 0 )
+         deregisterTimer( mp.msgData().msgId() );
+      else
+         registerTimer( mp, interval );
+
+      break;
+   default:
+      _FATAL_ERROR( __FILE__, __LINE__, "Unknown OSCode in message packet" );
+      break;
+   }
+}
+
+void MsgSysTimer::updateTime( const unsigned long long tickCount )
+{
+   //
+   // Save the new tick count ...
+   _TickCount = tickCount;
+
+   //
+   // Check the registered timers ...
+   checkTimers();
+}
+
+void MsgSysTimer::registerTimer( const MessagePacket &mp, const unsigned long interval )
+{
+   //
+   // Deregister the timer if it already existed ...
+   deregisterTimer( mp.msgData().msgId() );
+
+   //
+   // Create a new Map Entry 
+   MapEntry *mePtr = new MapEntry;
+   MessagePacket *mpPtr = new MessagePacket( mp );
+   mePtr->_TimerMessage = mpPtr;
+   mePtr->_Interval = interval;
+
+   //
+   // Create a new Queue Entry
+   QueueEntry qe;
+   qe._MapEntryPtr = mePtr;  // the map entry object is owned by the queue entry object
+   qe._ExpirationTickCount = _TickCount + interval;
+
+   //
+   // Add the queue entry to the priority queue ...
+   _TimerQueue.push( qe );
+
+   //
+   // Create a new entry in the timer message map ...
+   _TimerMsgMap[ mp.msgData().msgId() ] = mePtr;
+}
+
+void MsgSysTimer::deregisterTimer( const unsigned long mId )
+{
+   //
+   // Find the timer in the timer message map ...
+   map< unsigned long, MapEntry* >::iterator miter =
+                                               _TimerMsgMap.find( mId );
+
+   //
+   // If found ...
+   if ( miter != _TimerMsgMap.end() )
+   {
+      //
+      // Set the map entry object to have an interval
+      //  of zero, which will disable the map and queue entry 
+      MapEntry *meOldPtr = (*miter).second;
+      meOldPtr->_Interval = 0;
+
+      //
+      // remove the map entry from the timer msg. map ...
+      _TimerMsgMap.erase( miter );
+   }
+}
+
+void MsgSysTimer::deregisterTimersOfTask( const unsigned long tId )
+{
+   //
+   // Iterate through the TimerMsgMap and delete and obsolete all timers
+   //  which belong the the given tId ...
+   MapEntry *mePtr;
+   map< unsigned long, MapEntry* >::iterator miter;
+   for ( miter = _TimerMsgMap.begin() ; 
+         miter != _TimerMsgMap.end() ; 
+         miter++ )
+   {
+      mePtr = (*miter).second;
+      if (    mePtr->_TimerMessage 
+           && mePtr->_TimerMessage->msgData().taskId() == tId )
+      {
+         //
+         // deregister this timer ...
+         mePtr->_Interval = 0;
+         _TimerMsgMap.erase( miter );
+      }
+   }
+}
+
+void MsgSysTimer::checkTimers()
+{
+   // 
+   // Check the top of the priority queue for entries which
+   //  have expired ...
+   while ( _TimerQueue.top() <= _TickCount )
+   {
+      QueueEntry qe = _TimerQueue.top();
+      _TimerQueue.pop();
+
+      //
+      // If the timer is not disarmed...
+      if (    qe._MapEntryPtr 
+           && qe._MapEntryPtr->_Interval > 0 )
+      {
+         //
+         // Get the message packet to send off ...
+         MessagePacket *mpPtr = qe._MapEntryPtr->_TimerMessage;
+
+         //
+         // Reinsert the timer for its next interval ...
+         qe._ExpirationTickCount += qe._MapEntryPtr->_Interval;
+         _TimerQueue.push( qe );
+
+         //
+         // Send the message packet ...
+         unsigned int retries=0;
+         while (    mq_send( _RouterMQ, mpPtr, sizeof( MessagePacket ), 0 ) == ERROR
+                 && retries++ < MAX_NUM_RETRIES );
+         if ( retries == MAX_NUM_RETRIES )
+         {
+            char buffer[256];
+            sprintf( buffer,"Check timers - timer Id=%lx, send failed",
+                     mpPtr->msgData().msgId() );
+            _FATAL_ERROR( __FILE__, __LINE__, buffer );
+         }
+         
+      }
+      else if ( qe._MapEntryPtr )
+      {
+         //
+         // Delete the map entry object that the queue entry points to...
+         if ( qe._MapEntryPtr->_TimerMessage )
+            delete qe._MapEntryPtr->_TimerMessage;
+         delete qe._MapEntryPtr;
+
+      }
+   }
+}
+
+void MsgSysTimer::shutdown()
+{
+   //
+   // Close the queues ...
+   if ( _TimerMQ != (mqd_t)0 )
+   {
+      mq_close( _TimerMQ );
+      _TimerMQ = (mqd_t)0;
+   }
+
+   if ( _RouterMQ != (mqd_t)0 )
+   {
+      mq_close( _RouterMQ );
+      _RouterMQ = (mqd_t)0;
+   }
+
+}
+
+void MsgSysTimer::cleanup()
+{
+   //
+   // Remove the delete hook ...
+   taskDeleteHookDelete( (FUNCPTR) &MsgSysTimer::taskDeleteHook );
+
+   //
+   // Clean the list structures ...
+   _TimerMsgMap.empty();
+
+   MapEntry *mePtr;
+   while ( _TimerQueue.size() > 0 )
+   {
+      QueueEntry qe = _TimerQueue.top();
+      _TimerQueue.pop();
+
+      mePtr = qe._MapEntryPtr;
+      if ( mePtr)
+      {
+         if ( mePtr->_TimerMessage )
+            delete mePtr->_TimerMessage;
+         delete mePtr;
+      }
+   }
+
+   _TheTimer=0;
+   _TheTimerTid=0;
+}
+
+MsgSysTimer::QueueEntry::QueueEntry()
+ : _ExpirationTickCount( 0 ), 
+   _MapEntryPtr( 0 )
+{
+}
+
+MsgSysTimer::QueueEntry::QueueEntry( const QueueEntry &qe )
+ : _ExpirationTickCount( qe._ExpirationTickCount ), 
+   _MapEntryPtr( qe._MapEntryPtr )
+{
+}
+
+MsgSysTimer::QueueEntry::~QueueEntry()
+{
+}
+
+MsgSysTimer::QueueEntry &MsgSysTimer::QueueEntry::operator=( const MsgSysTimer::QueueEntry &qe ) 
+{
+   if ( &qe != this )
+   {
+      _ExpirationTickCount = qe._ExpirationTickCount;
+      _MapEntryPtr = qe._MapEntryPtr; 
+   }
+   return *this; 
+}
+
+int MsgSysTimer::QueueEntry::operator==( const MsgSysTimer::QueueEntry &qe ) const 
+{
+   if ( _ExpirationTickCount == qe._ExpirationTickCount )
+      return 1;
+   return 0;
+}
+
+int MsgSysTimer::QueueEntry::operator==( const unsigned long long l ) const 
+{
+   if ( _ExpirationTickCount == l )
+      return 1;
+   return 0;
+}
+
+int MsgSysTimer::QueueEntry::operator<( const MsgSysTimer::QueueEntry &qe ) const 
+{
+   return( _ExpirationTickCount < qe._ExpirationTickCount ); 
+}
+
+int MsgSysTimer::QueueEntry::operator<( const unsigned long long l ) const 
+{
+   return( _ExpirationTickCount < l );
+}
+
+int MsgSysTimer::QueueEntry::operator<=( const unsigned long long l ) const
+{
+   return ( _ExpirationTickCount <= l );
+}
+
+int MsgSysTimer::QueueEntry::operator>( const MsgSysTimer::QueueEntry &qe ) const 
+{
+   return( _ExpirationTickCount > qe._ExpirationTickCount ); 
+}
+
+int MsgSysTimer::QueueEntry::operator>( const unsigned long long l ) const 
+{
+   return( _ExpirationTickCount > l );
+}
+
+int MsgSysTimer::QueueEntry::operator>=( const unsigned long long l ) const
+{
+   return ( _ExpirationTickCount >= l );
+}

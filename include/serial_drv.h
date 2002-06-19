a8621d5135fb1c50a89f07842d9159ea641a6be1
Lindner, John <John.Lindner@Terumobct.com>
2002-06-18 17:31:13 -0800

    - corrected comments [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14117]

--- include/serial_drv.h
+++ include/serial_drv.h
@@ -1,10 +1,12 @@
 /*
  * Copyright (C) 2002 Gambro BCT, Inc.  All rights reserved.
  *
- * vxWorks serial port driver for Trima control CPU
+ * vxWorks serial port driver with support for Ampro LB-486
  *
  * $Header: K:/BCT_Development/vxWorks/Common/include/rcs/serial_drv.h 1.1 2002/06/07 21:42:31 jl11312 Exp jl11312 $
  * $Log: serial_drv.h $
+ * Revision 1.1  2002/06/07 21:42:31  jl11312
+ * Initial revision
  *
  */
 

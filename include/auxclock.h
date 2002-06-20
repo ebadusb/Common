db486da232e0711fac76ed4fe1f34c1a02b10617
pn02526 <a@b>
2002-06-19 20:59:28 -0800

    Make usable by either C or C++ programs. Modify some comments. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14122]

--- include/auxclock.h
+++ include/auxclock.h
@@ -3,6 +3,8 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/auxclock.h 1.6 2002/12/16 18:29:31Z jl11312 Exp ms10234 $
  * $Log: auxclock.h $
+ * Revision 1.2  2002/06/19 09:57:01  pn02526
+ * Change the typedef for the tick counter to long long; completely qualify other type declarations; correct spelling in comments.
  * Revision 1.1  2002/06/18 09:07:27  pn02526
  * Initial revision
  *
@@ -21,40 +23,92 @@
 typedef long long int rawTick;  /* Type of the auxClockTicks counter */
 
 /* Get the interrupt rate for the auxClockTicks counter. */
+#ifdef __cplusplus 
+extern "C" int auxClockRateGet();
+#else 
 int auxClockRateGet();
+#endif
 
 /* Initialize the auxClockTicks facility. Call at system initialization time. */
+#ifdef __cplusplus 
+extern "C" void auxClockInit();
+#else 
 void auxClockInit();
+#endif
 
 /* Enable the auxClockTicks counter. */
+#ifdef __cplusplus 
+extern "C" int auxClockEnable();
+#else 
 int auxClockEnable();
+#endif
 
 /* Get the current value of the auxClockTicks counter. */
+#ifdef __cplusplus 
+extern "C" rawTick auxClockTicksGet();
+#else 
 rawTick auxClockTicksGet();
+#endif
 
-/* Workaround for the lack of a %ll format in vxWorks' [sf]printf() */
+/* Convert the given rawTick value to a numeric string. */
+#ifdef __cplusplus 
+extern "C" char * rawTickString( char * /* String buffer pointer */, rawTick /* tick value to convert to string */ );
+#else 
 char * rawTickString( char * /* String buffer pointer */, rawTick /* tick value to convert to string */ );
+#endif
 
-/* Get the current value of the auxClockTicks counter as a decimal string. */
+/* Get the current value of the auxClockTicks counter as a numeric string. */
+#ifdef __cplusplus 
+extern "C" char *auxClockTicksString();
+#else 
 char *auxClockTicksString();
+#endif
+
 
 /* Enable the auxClock Message Packet queue to send auxClockTicks every given number of ticks. */
+#ifdef __cplusplus 
+extern "C" int auxClockMsgPktEnable( unsigned int /* number of ticks */,
+                          const char * /* Name of Message Packet queue to which to send auxClockTicks in a Message Packet */,
+                          int *        /* Flag for detecting overruns by the calling task */ );
+#else 
 int auxClockMsgPktEnable( unsigned int /* number of ticks */,
                           const char * /* Name of Message Packet queue to which to send auxClockTicks in a Message Packet */,
                           int *        /* Flag for detecting overruns by the calling task */ );
+#endif
+
 /* Post a receive for the auxClock Message Packet queue. */
+#ifdef __cplusplus 
+extern "C" int auxClockMsgPktReceive( mqd_t, rawTick * /* Pointer to buffer to put current value of the auxClock tick counter */ );
+#else 
 int auxClockMsgPktReceive( mqd_t, rawTick * /* Pointer to buffer to put current value of the auxClock tick counter */ );
+#endif
 
-/* Return the notification overrun count. */
+/* Return the semaphore/MsgQ overrun count. */
+#ifdef __cplusplus 
+extern "C" unsigned long int auxClockNotificationOverruns();
+#else 
 unsigned long int auxClockNotificationOverruns();
+#endif
 
-/* Return the notification count. */
+/* Return the semaphore Give/MsgQ Send count. */
+#ifdef __cplusplus 
+extern "C" unsigned long int auxClockNotifications();
+#else 
 unsigned long int auxClockNotifications();
+#endif
 
 /* Return the notification count when the first failure occurred. */
+#ifdef __cplusplus 
+extern "C" unsigned long int auxClockNotificationFailedAt();
+#else 
 unsigned long int auxClockNotificationFailedAt();
+#endif
 
 /* Return the errno value for the first notification failure that occurred. */
+#ifdef __cplusplus 
+extern "C" int auxClockNotificationErrno();
+#else 
 int auxClockNotificationErrno();
+#endif
 
 #endif /* !defined(AUXCLOCK_H) */

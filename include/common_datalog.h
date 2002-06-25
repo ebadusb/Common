e01373207f382dabdbab49581a56da13c21b5454
rm70006 <a@b>
2002-06-25 13:14:57 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14130]

---
+++ include/common_datalog.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2002 by Cobe BCT, Inc.  All rights reserved.
+ *
+ * TITLE:      common_datalog.h
+ *
+ * AUTHOR:     Regis McGarry
+ *
+ * ABSTRACT:   This header file contains definitions that are used
+ *              throughout the Common system to define logging levels and keys.
+ *
+ * DOCUMENTS
+ * Requirements:
+ * Test:
+ *
+ */
+
+
+#ifndef COMMON_DATALOG_H  //  Prevents multiple compilations.
+#define COMMON_DATALOG_H
+
+#include "datalog.h"
+
+//
+//
+//   Logging levels for programmer debug
+//
+#define LOG_DATASTORE           "log_datastore"
+
+#endif

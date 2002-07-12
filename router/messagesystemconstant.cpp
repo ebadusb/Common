57d07e0ec98c6870f1c2be7222c1c70b0b0d6b63
Scott, Mark <Mark.Scott@terumobct.com>
2002-07-11 18:57:27 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14159]

---
+++ router/messagesystemconstant.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2002 by Gambro BCT, Inc.  All rights reserved.
+ *
+ * TITLE:      messagesystemconstant.cpp
+ *
+ */
+
+#include <messagesystemconstant.h>
+
+const unsigned int MessageSystemConstant::DEFAULT_ROUTER_Q_SIZE=100;
+const unsigned int MessageSystemConstant::DEFAULT_Q_SIZE=30;
+const unsigned int MessageSystemConstant::MAX_MESSAGE_QUEUE_SIZE=400;
+const unsigned int MessageSystemConstant::CONNECT_DELAY=500; // milliseconds
+const unsigned int MessageSystemConstant::MAX_NUM_RETRIES=1;
+const struct timespec MessageSystemConstant::RETRY_DELAY={ 1 /* seconds */, 0 /*nanoseconds*/ };
+const int MessageSystemConstant::DEFAULT_MESSAGE_PRIORITY=15;
+const int MessageSystemConstant::DEFAULT_TIMER_MESSAGE_PRIORITY=16;
+

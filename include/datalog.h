4705bb1bb182f96df16be6ab91ffdf25b01302b9
Lindner, John <John.Lindner@Terumobct.com>
2002-07-17 18:31:51 -0800

    - initial datalog implementation (no support for periodic logging) [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14179]

--- include/datalog.h
+++ include/datalog.h
@@ -3,6 +3,9 @@
  *
  * $Header: K:/BCT_Development/vxWorks/Common/include/rcs/datalog.h 1.21 2003/02/25 20:40:08Z jl11312 Exp jl11312 $
  * $Log: datalog.h $
+ * Revision 1.8  2002/06/04 20:23:48  jl11312
+ * - added default level related functions
+ * - modified to compile correctly with C-source files
  * Revision 1.7  2002/05/17 18:27:59  jl11312
  * - more temporary changes for debug use under vxWorks
  * Revision 1.6  2002/05/17 17:15:59  jl11312
@@ -29,6 +32,25 @@
 #include "datalog_port.h"
 
 /*
+ *	Version information
+ */
+#define	DATALOG_MAJOR_VERSION	(1)
+#define	DATALOG_MINOR_VERSION	(1)
+
+/*
+ *	Datalog related data types
+ */
+typedef enum { DataLog_OK, DataLog_Error } DataLog_Result;
+typedef enum { DataLog_LogEnabled, DataLog_LogDisabled } DataLog_EnabledType;
+typedef enum { DataLog_ConsoleEnabled, DataLog_ConsoleDisabled } DataLog_ConsoleEnabledType;
+
+typedef DataLog_SharedPtr(struct DataLog_HandleInfo)	DataLog_Handle;
+typedef DataLog_SharedPtr(struct DataLog_SetInfo)		DataLog_SetHandle;
+
+#define DATALOG_NULL_HANDLE DATALOG_NULL_SHARED_PTR
+#define DATALOG_NULL_SET_HANDLE DATALOG_NULL_SHARED_PTR
+ 
+/*
  * C/C++ functions
  */
 #ifdef __cplusplus
@@ -38,9 +60,7 @@ extern "C" {
 /*
  * Data log initialization routines
  */
-typedef enum { DataLog_OK, DataLog_Error } DataLog_Result;
-
-DataLog_Result datalog_Init(const char * logPath);
+DataLog_Result datalog_Init(const char * logPath, const char * platformName);
 DataLog_Result datalog_InitNet(const char * ipAddress, double seconds);
 
 DataLog_Result datalog_SetDefaultTraceBufferSize(size_t size);
@@ -51,34 +71,20 @@ typedef DataLog_SharedPtr(DataLog_BufferData) DataLog_EncryptFunc(DataLog_Shared
 DataLog_Result datalog_SetEncryptFunc(DataLog_EncryptFunc * func);
 
 /*
- * Data log level creation
+ * Data log handle routines
  */
-typedef struct DataLog_HandleInfo * DataLog_Handle;
-#define NULL_DATALOG_HANDLE   NULL
-
 DataLog_Result datalog_CreateLevel(const char * levelName, DataLog_Handle * handle);
-DataLog_Result	datalog_SetDefaultLevel(DataLog_Handle handle);
-
-/*
- * Interrupt log level creation
- */
 DataLog_Result datalog_CreateIntLevel(const char * levelName, DataLog_Handle * handle);
-
-/*
- * Critical log level functions
- */
 DataLog_Handle datalog_GetCriticalHandle(void);
+DataLog_Result	datalog_SetDefaultLevel(DataLog_Handle handle);
 
 /*
  * Data log option control
  */
-typedef enum { LogEnabled, LogDisabled } DataLog_EnabledType;
-typedef enum { ConsoleEnabled, ConsoleDisabled } DataLog_ConsoleEnabledType;
-
 DataLog_Result datalog_GetTaskOutputOptions(DataLog_TaskID task, DataLog_EnabledType * log, DataLog_ConsoleEnabledType * console);
 DataLog_Result datalog_SetTaskOutputOptions(DataLog_TaskID task, DataLog_EnabledType log, DataLog_ConsoleEnabledType console);
-DataLog_Result datalog_GetLevelOptions(DataLog_Handle handle, DataLog_EnabledType * log, DataLog_ConsoleEnabledType * console);
-DataLog_Result datalog_SetLevelOptions(DataLog_Handle handle, DataLog_EnabledType log, DataLog_ConsoleEnabledType console);
+DataLog_Result datalog_GetLevelOutputOptions(DataLog_Handle handle, DataLog_EnabledType * log, DataLog_ConsoleEnabledType * console);
+DataLog_Result datalog_SetLevelOutputOptions(DataLog_Handle handle, DataLog_EnabledType log, DataLog_ConsoleEnabledType console);
 
 /*
  * printf-like interface for data log output
@@ -89,31 +95,30 @@ DataLog_Result datalog_PrintToDefault(const char * file, int line, const char * 
 /*
  * periodic logging routines
  */
-typedef struct DataLog_SetInfo * DataLog_Set;
-#define NULL_DATALOG_SET   NULL
-DataLog_Result datalog_CreatePeriodicSet(const char * setName, DataLog_Set * handle);
+DataLog_Result datalog_CreatePeriodicSet(const char * setName, DataLog_SetHandle * handle);
 
-void datalog_AddPtr(DataLog_Set handle, void * ptr, size_t size, const char * key, const char * description);
-void datalog_AddCharPtr(DataLog_Set handle, const char * const * ptr, const char * key, const char * description);
-void datalog_AddIntFunc(DataLog_Set handle, int (* func)(void *), void * arg, const char * key, const char * description);
-void datalog_AddLongFunc(DataLog_Set handle, long (* func)(void *), void * arg, const char * key, const char * description);
-void datalog_AddDoubleFunc(DataLog_Set handle, double (* func)(void *), void * arg, const char * key, const char * description);
-void datalog_AddCharPtrFunc(DataLog_Set handle, const char * (* func)(void *), void * arg, const char * key, const char * description);
+void datalog_AddPtr(DataLog_SetHandle handle, void * ptr, size_t size, const char * key, const char * description);
+void datalog_AddCharPtr(DataLog_SetHandle handle, const char * const * ptr, const char * key, const char * description);
+void datalog_AddIntFunc(DataLog_SetHandle handle, int (* func)(void *), void * arg, const char * key, const char * description);
+void datalog_AddLongFunc(DataLog_SetHandle handle, long (* func)(void *), void * arg, const char * key, const char * description);
+void datalog_AddDoubleFunc(DataLog_SetHandle handle, double (* func)(void *), void * arg, const char * key, const char * description);
+void datalog_AddCharPtrFunc(DataLog_SetHandle handle, const char * (* func)(void *), void * arg, const char * key, const char * description);
 
-DataLog_Result datalog_GetPeriodicOutputInterval(DataLog_Set handle, double * seconds);
-DataLog_Result datalog_SetPeriodicOutputInterval(DataLog_Set handle, double seconds);
-DataLog_Result datalog_ForcePeriodicOutput(DataLog_Set handle);
+DataLog_Result datalog_GetPeriodicOutputInterval(DataLog_SetHandle handle, double * seconds);
+DataLog_Result datalog_SetPeriodicOutputInterval(DataLog_SetHandle handle, double seconds);
+DataLog_Result datalog_ForcePeriodicOutput(DataLog_SetHandle handle);
 
 /*
  * error interface
  */
-DataLog_ErrorType datalog_Error(DataLog_TaskID task);
+DataLog_Result datalog_Error(DataLog_TaskID task, DataLog_ErrorType * error);
+DataLog_Result datalog_ClearError(DataLog_TaskID task);
 const char * datalog_ErrorMessage(DataLog_ErrorType error);
-void datalog_ClearError(DataLog_TaskID task);
+int datalog_ErrorContinuable(DataLog_ErrorType error);
 
-typedef void DataLog_AppErrorHandler(const char * file, int line, DataLog_ErrorType error, const char * msg, int continuable);
-DataLog_Result datalog_GetAppErrorHandler(DataLog_TaskID task, DataLog_AppErrorHandler ** func);
-DataLog_Result datalog_SetAppErrorHandler(DataLog_TaskID task, DataLog_AppErrorHandler * func);
+typedef void DataLog_TaskErrorHandler(const char * file, int line, DataLog_ErrorType error, const char * msg, int continuable);
+DataLog_Result datalog_GetTaskErrorHandler(DataLog_TaskID task, DataLog_TaskErrorHandler ** func);
+DataLog_Result datalog_SetTaskErrorHandler(DataLog_TaskID task, DataLog_TaskErrorHandler * func);
 
 DataLog_Result datalog_GetBytesBuffered(size_t * byteCount);
 DataLog_Result datalog_GetBytesWritten(size_t * byteCount);
@@ -128,8 +133,20 @@ DataLog_Result datalog_GetBytesMissed(size_t * byteCount);
  */
 #ifdef __cplusplus
 
-// #include "logstream.h"
-#include <iostream.h>
+#include <strstream.h>
+
+class DataLog_OutputBuffer;
+class DataLog_Stream : public ostrstream
+{
+	friend class DataLog_OutputBuffer;
+	friend ostream & endmsg(ostream & stream);
+
+private:
+	DataLog_Stream(DataLog_BufferData * buffer, size_t bufferSize, DataLog_OutputBuffer * output);
+	virtual ~DataLog_Stream() {}
+
+	DataLog_OutputBuffer * _output;
+};
 
 class DataLog_Level
 {
@@ -141,28 +158,28 @@ public:
 
    virtual ~DataLog_Level();
 
-	DataLog_EnabledType taskLogFlag(DataLog_TaskID task);
-	DataLog_EnabledType taskLogFlag(DataLog_TaskID task, DataLog_EnabledType flag);
+	DataLog_EnabledType logOutput(void);
+	DataLog_EnabledType logOutput(DataLog_EnabledType flag);
 
-	DataLog_ConsoleEnabledType taskConsoleFlag(DataLog_TaskID task);
-	DataLog_ConsoleEnabledType taskConsoleFlag(DataLog_TaskID task, DataLog_ConsoleEnabledType flag);
+	DataLog_ConsoleEnabledType consoleOutput(void);
+	DataLog_ConsoleEnabledType consoleOutput(DataLog_ConsoleEnabledType flag);
 
-	DataLog_EnabledType globalLogFlag(void);
-	DataLog_EnabledType globalLogFlag(DataLog_EnabledType flag);
+	DataLog_Stream & operator()(const char * fileName, int lineNumber);
 
-	DataLog_ConsoleEnabledType globalConsoleFlag(void);
-	DataLog_ConsoleEnabledType globalConsoleFlag(DataLog_ConsoleEnabledType flag);
+	DataLog_Handle getHandle(void);
 
-   ostream & operator()(const char * fileName, int lineNumber);
+protected:
+	DataLog_Handle	_handle;
 };
 
 class DataLog_DefaultLevel
 {
 public:
-   static ostream & getStream(const char * fileName, int lineNumber);
+   static DataLog_Stream & getStream(const char * fileName, int lineNumber);
 };
 
-#define endmsg endl
+ostream & endmsg(ostream & stream);
+//ostream & operator << (ostream & stream, ostream & (*manip)( &));
 
 class DataLog_Critical : public DataLog_Level
 {
@@ -174,13 +191,13 @@ public:
 #define DataLog(instance) (instance)(__FILE__, __LINE__)
 #define DataLog_Default DataLog_DefaultLevel::getStream(__FILE__, __LINE__)
 
-template<class Value> inline DataLog_Result datalog_AddRef(DataLog_Set handle, const Value& ref, const char * key, const char * description)
+template<class Value> inline DataLog_Result datalog_AddRef(DataLog_SetHandle handle, const Value& ref, const char * key, const char * description)
    {
       printf("datalog_AddRef key = %s, description = %s\n", key, description);
       return DataLog_OK;
    }
 
-template<class Value, class Arg> inline DataLog_Result datalog_AddFunc(DataLog_Set handle, Value (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
+template<class Value, class Arg> inline DataLog_Result datalog_AddFunc(DataLog_SetHandle handle, Value (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
    {
       printf("datalog_AddFunc key = %s, description = %s\n", key, description);
       return DataLog_OK;
@@ -191,30 +208,58 @@ template<class Value, class Arg> inline DataLog_Result datalog_AddFunc(DataLog_S
  * handled correctly
  */
 typedef char * DataLog_CharPtr;
-template<> inline DataLog_Result datalog_AddRef<DataLog_CharPtr>(DataLog_Set handle, const DataLog_CharPtr& ref, const char * key, const char * description)
+template<> inline DataLog_Result datalog_AddRef<DataLog_CharPtr>(DataLog_SetHandle handle, const DataLog_CharPtr& ref, const char * key, const char * description)
    {
       printf("datalog_AddRef(char *) key = %s, description = %s\n", key, description);
       return DataLog_OK;
    }
 
-template<class Arg> inline DataLog_Result datalog_AddFunc(DataLog_Set handle, DataLog_CharPtr (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
+template<class Arg> inline DataLog_Result datalog_AddFunc(DataLog_SetHandle handle, DataLog_CharPtr (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
    {
       printf("datalog_AddFunc(char *) key = %s, description = %s\n", key, description);
       return DataLog_OK;
    }
 
 typedef const char * DataLog_ConstCharPtr;
-template<> inline DataLog_Result datalog_AddRef<DataLog_ConstCharPtr>(DataLog_Set handle, const DataLog_ConstCharPtr& ref, const char * key, const char * description)
+template<> inline DataLog_Result datalog_AddRef<DataLog_ConstCharPtr>(DataLog_SetHandle handle, const DataLog_ConstCharPtr& ref, const char * key, const char * description)
    {
       printf("datalog_AddRef(const char *) key = %s, description = %s\n", key, description);
       return DataLog_OK;
    }
 
-template<class Arg> inline DataLog_Result datalog_AddFunc(DataLog_Set handle, DataLog_ConstCharPtr (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
+template<class Arg> inline DataLog_Result datalog_AddFunc(DataLog_SetHandle handle, DataLog_ConstCharPtr (* func)(const Arg& arg), const Arg& arg, const char * key, const char * description)
    {
       printf("datalog_AddFunc(const char *) key = %s, description = %s\n", key, description);
       return DataLog_OK;
    }
+
+class DataLog_OutputTask
+{
+public:
+	DataLog_OutputTask(const char * platformName);
+	virtual ~DataLog_OutputTask() {}
+
+	int main(void);
+	void pause(void) { _isRunning = false; }
+	void resume(void) { _isRunning = true; }
+	void exit(int code);
+
+private:
+	void notifyNetworkBufferSize(size_t size);
+	void shutdown(void);
+	void writeLogFileHeader(const char * platformName);
+	void writeMissedLogDataRecord(void);
+	void writeOutputRecord(DataLog_BufferData * buffer, size_t size);
+	void writeFileCloseRecord(void);
+	void writeTimeStampRecord(void);
+
+private:
+	bool _isRunning;
+	bool _isExiting;
+	int  _exitCode;
+	int _outputFile;
+	DataLog_SharedPtr(DataLog_CommonData) _common;
+};
 
 #endif /* ifdef __cplusplus */
 #endif /* ifndef _DATALOG_INCLUDE */
--- include/datalog_port.h
+++ include/datalog_port.h
@@ -3,6 +3,9 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/datalog_port.h 1.16 2003/10/03 12:32:57Z jl11312 Exp rm70006 $
  * $Log: datalog_port.h $
+ * Revision 1.4  2002/06/04 20:23:48  jl11312
+ * - added default level related functions
+ * - modified to compile correctly with C-source files
  * Revision 1.3  2002/04/29 21:48:42  jl11312
  * - updated to reflect API changes
  * Revision 1.2  2002/03/27 16:43:25  jl11312
@@ -22,8 +25,49 @@
  */
 typedef enum
 {
-	DataLog_NoError
+	DataLog_NoError = 0,			/* must be first entry */
+
+	DataLog_BadNetworkClientData,
+	DataLog_MultipleInitialization,
+	DataLog_NoSuchTask,
+	DataLog_InvalidHandle,
+	DataLog_LevelNotInitialized,
+	DataLog_CriticalBufferMissing,
+	DataLog_LevelConstructorFailed,
+	DataLog_OpenOutputFileFailed,
+	DataLog_LevelRecordWriteFailed,
+	DataLog_PrintFormatError,
+	DataLog_InternalWriteError,
+
+	DataLog_LastError				/* must be last entry */
 } DataLog_ErrorType;
+
+#ifdef DATALOG_DECLARE_ERROR_INFORMATION
+
+typedef struct
+{
+	DataLog_ErrorType	_error;
+	const char * _message;
+	int _continuable;
+} DataLog_ErrorInformation;
+
+DataLog_ErrorInformation	datalog_ErrorInformation[DataLog_LastError] =
+{
+	{ DataLog_NoError, "no error", 1 },
+	{ DataLog_BadNetworkClientData, "bad network client data", 1 },
+	{ DataLog_MultipleInitialization, "multiple initialization", 1 },
+	{ DataLog_NoSuchTask, "no such task", 1 },
+	{ DataLog_InvalidHandle, "invalid handle", 1 },
+	{ DataLog_LevelNotInitialized, "level not initialized", 1 },
+	{ DataLog_CriticalBufferMissing, "critical buffer missing", 0 },
+	{ DataLog_LevelConstructorFailed, "level constructor failed", 0 },
+	{ DataLog_OpenOutputFileFailed, "open output file failed", 0 },
+	{ DataLog_LevelRecordWriteFailed, "level record write failed", 1 },
+	{ DataLog_PrintFormatError, "print format error", 1 },
+	{ DataLog_InternalWriteError, "internal write error", 1 }
+};
+
+#endif /* ifdef DATALOG_DECLARE_ERROR_INFORMATION */
 
 /*
  * Platform specific configuration
@@ -31,17 +75,141 @@ typedef enum
 #ifdef VXWORKS
 
 /*
- * Trima on vxWorks platform
+ *	Platform specific data types
+ */
+typedef unsigned char DataLog_UINT8;
+typedef unsigned short DataLog_UINT16;
+typedef unsigned long DataLog_UINT32;
+ 
+/*
+ *	Task related definitions
  */
 typedef int DataLog_TaskID;
 #define DATALOG_CURRENT_TASK 0
 
+/*
+ *	Shared memory related definitions
+ */
 #define DataLog_SharedPtr(type) type *
+#define DATALOG_NULL_SHARED_PTR NULL
 
+/*
+ *	Output buffer related definitions
+ */
 typedef unsigned char DataLog_BufferData;
 
-#else
-#error "Unknown platform - no definition for DataLog_TaskID type"
-#endif
+/*
+ *	Access lock related definitions
+ */
+#include <semLib.h>
+typedef SEM_ID	DataLog_Lock;
+
+/*
+ *	STL list type
+ */
+#ifdef __cplusplus
+#include <list.h>
+#define	DataLog_List	list
+#endif /* ifdef __cplusplus */
+
+/*
+ *	STL map type
+ */
+#ifdef __cplusplus
+#include <map.h>
+#define	DataLog_Map		map
+#endif /* ifdef __cplusplus */
+
+/*
+ *	Network related definitions
+ */
+#ifndef DATALOG_NO_NETWORK_SUPPORT
+typedef unsigned long	DataLog_NodeID;
+#endif /* ifndef DATALOG_NO_NETWORK_SUPPORT */
+
+#else /* ifdef VXWORKS */
+#error "Unknown platform"
+#endif /* ifdef VXWORKS */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* ifdef __cplusplus */
+
+/*
+ *	Task related functions
+ */
+DataLog_TaskID datalog_CurrentTask(void);
+const char * datalog_TaskName(DataLog_TaskID taskID);
+
+/*
+ *	Shared memory related functions
+ */
+DataLog_SharedPtr(void) datalog_AllocSharedMem(size_t size);
+void datalog_FreeSharedMem(DataLog_SharedPtr(void) ptr);
+
+/*
+ *	Access lock related functions
+ */
+DataLog_Lock datalog_CreateLock(void);
+void datalog_DeleteLock(DataLog_Lock lock);
+void datalog_LockAccess(DataLog_Lock lock);
+void datalog_ReleaseAccess(DataLog_Lock lock);
+
+/*
+ *	Signal related functions
+ */
+#ifdef __cplusplus
+bool datalog_WaitSignal(const char * signalName, double seconds);
+void datalog_SendSignal(const char * signalName);
+#endif /* ifdef __cplusplus */
+
+/*
+ *	Comman data area related functions
+ */
+#ifdef __cplusplus
+class DataLog_CommonData;
+DataLog_SharedPtr(DataLog_CommonData) datalog_GetCommonDataPtr(void);
+void datalog_SetCommonDataPtr(DataLog_SharedPtr(DataLog_CommonData) ptr);
+bool datalog_StartInitialization(void);
+#endif /* ifdef __cplusplus */
+
+/*
+ *	Data log task related functions
+ */
+void datalog_StartOutputTask(const char * platformName);
+
+/*
+ *	Time stamp related functions.  Note that the related structures
+ * are written to the log file and so must be packed.
+ */
+typedef struct
+{
+	DataLog_UINT8	_day __attribute__ ((packed));		/* 1-31 */
+	DataLog_UINT8	_month __attribute__ ((packed));		/* 1-12 */
+	DataLog_UINT16	_year __attribute__ ((packed));
+	DataLog_UINT8	_hour __attribute__ ((packed));
+	DataLog_UINT8	_minute __attribute__ ((packed));
+	DataLog_UINT8	_second __attribute__ ((packed));
+} DataLog_TimeStampStart;
+
+typedef struct
+{
+	DataLog_UINT32	_seconds __attribute__ ((packed));
+	DataLog_UINT32	_nanoseconds __attribute__ ((packed));
+} DataLog_TimeStamp;
+
+void datalog_GetTimeStampStart(DataLog_TimeStampStart * start);
+void datalog_GetTimeStamp(DataLog_TimeStamp * stamp);
+
+void datalog_TaskCreated(DataLog_TaskID taskID);
+void datalog_TaskDeleted(DataLog_TaskID taskID);
+
+#ifndef DATALOG_NO_NETWORK_SUPPORT
+DataLog_NodeID datalog_NodeID(void);
+#endif /* ifndef DATALOG_NO_NETWORK_SUPPORT */
+
+#ifdef __cplusplus
+}; // extern "C"
+#endif /* ifdef __cplusplus */
 
 #endif /* ifndef _DATALOG_PORT_INCLUDE */

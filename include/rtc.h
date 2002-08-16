39938ff1489441b6498a883001ac81c390f798cb
rm70006 <a@b>
2002-08-16 14:52:12 -0800

    Include vxWorks in header. STATUS depends on it. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14218]

--- include/rtc.h
+++ include/rtc.h
@@ -5,6 +5,8 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/rtc.h 1.4 2002/10/08 15:35:17Z jl11312 Exp ms10234 $
  * $Log: rtc.h $
+ * Revision 1.2  2002/06/04 20:24:26Z  jl11312
+ * - added protection for multiple includes
  * Revision 1.1  2002/05/30 13:00:30  jl11312
  * Initial revision
  *
@@ -12,6 +14,8 @@
 
 #ifndef _RTC_INCLUDE
 #define _RTC_INCLUDE
+
+#include "vxWorks.h"    // Definition of STATUS
 
 STATUS setCurrentTimeFromRTC(void);
 STATUS setRTCFromCurrentTime(void);

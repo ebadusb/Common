650cc70850518345fb23aee726be68ee902ef478
rm70006 <a@b>
2002-08-23 12:53:51 -0800

    changed binditem to work with 486 compiler bug [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14228]

--- include/datastore_private.h
+++ include/datastore_private.h
@@ -12,6 +12,8 @@
  *             only by datastore.h
  *
  * HISTORY:    $Log: datastore_private.h $
+ * HISTORY:    Revision 1.3  2002/07/16 21:05:05Z  rm70006
+ * HISTORY:    Fix bug in check for multiple writers.
  * HISTORY:    Revision 1.2  2002/06/24 20:37:26Z  rm70006
  * HISTORY:    Make max number of symbols 512 from 128.
  * HISTORY:    Revision 1.1  2002/06/24 19:30:42Z  rm70006
@@ -29,7 +31,58 @@
 #include "datastore.h"
 
 #include "error.h"
-#include <a_out.h>
+#include <a_out.h>  // Needed for symbol table
+
+
+
+////////////////////////////////////////////////////////////////
+// BindItem
+////////////////////////////////////////////////////////////////
+
+//template <class T> void DataStore::BindItem(T** dataPtr, BIND_ITEM_TYPE item, bool &created)
+template <class T> void BindItem(DataStore *ds, T **dataPtr, BIND_ITEM_TYPE item, bool &created)
+{
+   //char nameKey[SYM_NAME_SIZE];
+   string nameKey(SYM_NAME_SIZE, ' ');
+
+   SYM_TYPE symbolType;
+   char *valPtr;
+
+   ds->GetSymbolName (nameKey, item);
+
+   // Search for Symbol.
+   STATUS status = symFindByName(DataStore::getTable(), (char *)nameKey.c_str(), &valPtr, &symbolType);
+
+   // If symbol exists
+   if (status == OK)
+   {
+      // return a pointer to the value
+      *dataPtr = (T *)valPtr;
+      created = false;
+
+      DataLog(*(ds->_debug)) << "Attaching item " << nameKey.c_str() << ", address " << dataPtr << ", " << *dataPtr << endmsg;
+   }
+   else
+   {
+      // create a new data element
+      *dataPtr = new T; 
+
+      // add to symbol table
+      status = symAdd(DataStore::getTable(), (char *)nameKey.c_str(), (char *)*dataPtr, N_DATA, item);
+
+      //DataLog(*(ds->_debug)) << "Creating item " << nameKey.c_str() << ", address " << dataPtr << ", " << *dataPtr << endmsg;
+
+      if (status == ERROR)
+      {
+         // Log Fatal Error
+         DataLog(*(ds->_fatal)) << "BindItem: symAdd failed in CDS " << ds->Name() << "." << endmsg;
+         _FATAL_ERROR(__FILE__, __LINE__, "FATAL ERROR");
+      }
+
+      created = true;
+   }
+}
+
 
 
 ////////////////////////////////////////////////////////////////
@@ -68,10 +121,16 @@ template <class dataType> void BaseElement<dataType>::Register (DataStore *ds, R
    ElementType::Register(ds, role, pfr);
 
    // Bind the data element to the DataStore symbol table entry.
-   ds->BindItem(&_data, ITEM_DATA, created);
+   BindItem(ds, &_data, ITEM_DATA, created);
+
+   // Only add the element if PFR is desired and it didn't exist in the list (avoid duplicates).
+   if ( created && (_pfrType == PFR_RECOVER) )
+   {
+      _ds->AddElement(this);
+   }
 
    // Bind the spoof fp to the DataStore symbol table entry.
-   ds->BindItem(&_fp, ITEM_SPOOF, created);
+   BindItem(ds, &_fp, ITEM_SPOOF, created);
    *_fp = NULL;
 }
 
@@ -87,17 +146,26 @@ template <class dataType> void BaseElement<dataType>::Register (DataStore *ds, R
    ElementType::Register(ds, role, pfr);
 
    // Bind the data element to the DataStore symbol table entry.
-   ds->BindItem(&_data, ITEM_DATA, created);
+   BindItem(ds, &_data, ITEM_DATA, created);
+
+   if (created)
+   {
+      // Only add the element if PFR is desired and it didn't exist in the list (avoid duplicates).
+      if (_pfrType == PFR_RECOVER)
+      {
+         _ds->AddElement(this);
+      }
+
+      // Initialize Element if able
+      if (_role == ROLE_RW)
+      {
+         Set(initValue);
+      }
+   }
 
    // Bind the spoof fp to the DataStore symbol table entry.
-   ds->BindItem(&_fp, ITEM_SPOOF, created);
+   BindItem(ds, &_fp, ITEM_SPOOF, created);
    *_fp = NULL;
-
-   // Only initialize if Role is not RO
-   if (_role != ROLE_RO)
-   {
-      *_data = initValue;
-   }
 }
 
 
@@ -134,7 +202,7 @@ template <class dataType> bool BaseElement<dataType>::Set(const dataType &data)
    else
    {
       // Log Fatal Error
-      DataLog(_ds->_fatal) << "BaseElement: Set Failed.  Role is RO." << endmsg;
+      DataLog(*(_ds->_fatal)) << "BaseElement: Set Failed in CDS " << _ds->Name() << ".  Role is RO." << endmsg;
       _FATAL_ERROR(__FILE__, __LINE__, "FATAL ERROR");
       return false;
    }
@@ -149,7 +217,7 @@ template <class dataType> void BaseElement<dataType>::ReadSelf (ifstream &pfrfil
 {
    pfrfile.read(_data, sizeof(dataType));
    //pfrfile >> _data;
-   //DataLog(_ds->_debug) << "Reading value " << *_data << ", size " << sizeof(dataType) << endmsg;
+   //DataLog(_ds->*_debug) << "Reading value " << *_data << ", size " << sizeof(dataType) << endmsg;
 }
    
 
@@ -159,7 +227,7 @@ template <class dataType> void BaseElement<dataType>::ReadSelf (ifstream &pfrfil
 //
 template <class dataType> void BaseElement<dataType>::WriteSelf (ofstream &pfrfile)
 {
-   //DataLog(_ds->_debug) << "Writing value " << *_data << ", size " << sizeof(dataType) << endmsg;
+   //DataLog(_ds->*_debug) << "Writing value " << *_data << ", size " << sizeof(dataType) << endmsg;
    pfrfile.write(_data, sizeof(dataType));
    //pfrfile << _data;
 }
@@ -216,17 +284,7 @@ template <class dataType> RangedElement<dataType>::~RangedElement()
 //
 template <class dataType> void RangedElement<dataType>::Register (DataStore *ds, Role role, PfrType pfr, const dataType min, const dataType max)
 {
-   bool created; 
-
-   // Call the base class register
-   ElementType::Register(ds, role, pfr);
-
-   // Bind the data element to the DataStore symbol table entry.
-   ds->BindItem(&_data, ITEM_DATA, created);
-
-   // Bind the spoof fp to the DataStore symbol table entry.
-   ds->BindItem(&_fp, ITEM_SPOOF, created);
-   *_fp = NULL;
+   BaseElement::Register(ds, role, pfr);
 
    _min = min;
    _max = max;
@@ -239,45 +297,10 @@ template <class dataType> void RangedElement<dataType>::Register (DataStore *ds,
 //
 template <class dataType> void RangedElement<dataType>::Register (DataStore *ds, Role role, PfrType pfr, const dataType min, const dataType max, const dataType &initValue)
 {
-   bool created;
-
-   // Call the base class register
-   ElementType::Register(ds, role, pfr);
-   
-   // Bind the data element to the DataStore symbol table entry.
-   ds->BindItem(&_data, ITEM_DATA, created);
-
-   // Bind the spoof fp to the DataStore symbol table entry.
-   ds->BindItem(&_fp, ITEM_SPOOF, created);
-   *_fp = NULL;
+   BaseElement::Register(ds, role, pfr, initValue);
 
    _min = min;
    _max = max;
-
-      // Only initialize if Role is not RO
-   if (_role != ROLE_RO)
-   {
-      *_data = initValue;
-   }
-}
-
-
-
-//
-// Get method
-//
-template <class dataType> const dataType & RangedElement<dataType>::Get() const
-{
-   // If calling instance is spoofer or no spoof has been registered, return real value
-   if ( (_role == ROLE_SPOOFER) || (*_fp == NULL) )
-   {
-      return *_data;
-   }
-   else
-   {
-      FP fp = *_fp;
-      return (fp(*this));   // Give control to spoofer to return "spoofed" value.
-   }
 }
 
 
@@ -290,46 +313,15 @@ template <class dataType> bool RangedElement<dataType>::Set(const dataType &data
    if ( (data >= _min) && 
         (data <= _max) )
    {
-      if (_role != ROLE_RO)
-      {
-         *_data = data;
-         return true;
-      }
-      else
-      {
-         // Log Fatal Error
-         DataLog(_ds->_fatal) << "RangedElement: Set Failed.  Role is RO.  Value=" << data << endmsg;
-         _FATAL_ERROR(__FILE__, __LINE__, "FATAL ERROR");
-         return false;
-      }
+      return Set(data);
    }
    else
    {
       // Log Error
-      DataLog(_ds->_debug) << "RangedElement: Set Failed.  Value is out of Range.  Value=" << data 
+      DataLog(*(_ds->_debug)) << "RangedElement: Set Failed.  Value is out of Range.  Value=" << data 
            << " Range=" << _min << "->" << _max << endmsg;
       return false;
    }
-}
-
-
-
-//
-// ReadSelf method
-//
-template <class dataType> void RangedElement<dataType>::ReadSelf (ifstream &pfrfile)
-{
-   BaseElement::ReadSelf(pfrfile);
-}
-
-
-
-//
-// WriteSelf method
-//
-template <class dataType> void RangedElement<dataType>::WriteSelf (ofstream &pfrfile)
-{
-   BaseElement::WriteSelf(pfrfile);
 }
 
 
@@ -353,87 +345,21 @@ const char DATASTORE_WRITER_DECLARED[] = "_DataStore_%s_writer_declared";
 const int DATASTORE_SYMTBL_SIZE = 9;
 
 
-
+/*
 //
-// BindItem
+// Debug routine to dump symbol table entry
 //
-template <class T> void DataStore::BindItem(T** dataPtr, BIND_ITEM_TYPE item, bool &created)
+BOOL DumpEntry(char *name, int val, SYM_TYPE type, int arg, UINT16 group)
 {
-   char nameKey[SYM_NAME_SIZE];
-   SYM_TYPE symbolType;
-   char *valPtr;
+   cout << "Symbol " << name << ", val " << val << ", type " << type
+        << ", group " << group << endl;
 
-
-   // Create the Symbol name to search for.
-   switch (item)
-   {
-   case ITEM_DATA:
-      sprintf(nameKey, DATASTORE_DATA_NAME, _name.c_str(), _refCount);
-      ++_refCount;
-      break;
-
-   case ITEM_SPOOF:
-      sprintf(nameKey, DATASTORE_SPOOF_NAME, _name.c_str(), _spoofCount);
-      ++_spoofCount;
-      break;
-
-   case ITEM_PFR_LIST:
-      sprintf(nameKey, DATASTORE_LIST_NAME, _name.c_str());
-      break;
-
-   case ITEM_READ_COUNT_SEMAPHORE:
-      sprintf(nameKey, DATASTORE_READ_SEM, _name.c_str());
-      break;
-
-   case ITEM_WRITE_COUNT_SEMAPHORE:
-      sprintf(nameKey, DATASTORE_WRITE_SEM, _name.c_str());
-      break;
-
-   case ITEM_SIGNAL_WRITE:
-      sprintf(nameKey, DATASTORE_SIGNAL_WRITE, _name.c_str());
-      break;
-
-   case ITEM_READ_COUNT:
-      sprintf(nameKey, DATASTORE_READ_COUNT, _name.c_str());
-      break;
-   
-   case ITEM_WRITER_DECLARED:
-      sprintf(nameKey, DATASTORE_WRITER_DECLARED, _name.c_str());
-      break;
-   }
-
-   // Search for Symbol.
-   STATUS status = symFindByName(_datastoreTable, nameKey, &valPtr, &symbolType);
-
-   // If symbol exists
-   if (status == OK)
-   {
-      // return a pointer to the value
-      *dataPtr = (T *)valPtr;
-      created = false;
-   }
-   else
-   {
-      // create a new data element (new char[size])
-      *dataPtr = new T; 
-
-      if (*dataPtr == NULL)
-      {
-         // Log Fatal Error
-         DataLog(_fatal) << "new allocator failed" << endmsg;
-         _FATAL_ERROR(__FILE__, __LINE__, "FATAL ERROR");
-      }
-      
-      // add to symbol table
-      status = symAdd(_datastoreTable, nameKey, (char *)*dataPtr, N_DATA, item);
-
-      if (status == ERROR)
-      {
-         // Log Fatal Error
-         DataLog(_fatal) << "symAdd failed" << endmsg;
-         _FATAL_ERROR(__FILE__, __LINE__, "FATAL ERROR");
-      }
-
-      created = true;
-   }
+   return true;
 }
+
+
+void dump_datastore_table ()
+{
+   symEach (DataStore::getTable(), DumpEntry, 0);
+}
+*/

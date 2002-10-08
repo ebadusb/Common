94052580f7df570a4cd66151eb6464bea39370e4
Lindner, John <John.Lindner@Terumobct.com>
2002-10-08 13:35:17 -0800

    - added ifdefs for C++ compile [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14299]

--- include/rtc.h
+++ include/rtc.h
@@ -5,6 +5,8 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/rtc.h 1.4 2002/10/08 15:35:17Z jl11312 Exp ms10234 $
  * $Log: rtc.h $
+ * Revision 1.3  2002/08/16 16:52:12  rm70006
+ * Include vxWorks in header.  STATUS depends on it.
  * Revision 1.2  2002/06/04 20:24:26Z  jl11312
  * - added protection for multiple includes
  * Revision 1.1  2002/05/30 13:00:30  jl11312
@@ -17,8 +19,16 @@
 
 #include "vxWorks.h"    // Definition of STATUS
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* ifdef __cplusplus */
+
 STATUS setCurrentTimeFromRTC(void);
 STATUS setRTCFromCurrentTime(void);
+
+#ifdef __cplusplus
+}; // extern "C"
+#endif /* ifdef __cplusplus */
 
 #endif /* ifndef _RTC_INCLUDE */
 

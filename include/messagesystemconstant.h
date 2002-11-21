8d64004b36febc8cf0d02836d3977de0b83f933c
rm70006 <a@b>
2002-11-20 16:09:38 -0800

    Change MAX_MESSAGE_SIZE to be an enum [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14348]

--- include/messagesystemconstant.h
+++ include/messagesystemconstant.h
@@ -43,7 +43,7 @@ public:
    static const int DEFAULT_TIMER_MESSAGE_PRIORITY;
    //
    // Size of the user data inside the message packets
-   static const int MAX_MESSAGE_SIZE=127;
+   enum {MAX_MESSAGE_SIZE = 127};
 
 };
 

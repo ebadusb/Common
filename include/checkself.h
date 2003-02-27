f98f530613845fce281a094aa50175343cc07d3a
td07711 <a@b>
2003-02-26 22:03:05 -0800

    mods for vxworks [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14422]

--- include/checkself.h
+++ include/checkself.h
@@ -6,6 +6,8 @@
  * CHANGELOG:
  * $Header: I:/BCT_Development/vxWorks/Common/include/rcs/checkself.h 1.3 2003/02/26 23:03:05Z td07711 Exp td07711 $
  * $Log: checkself.h $
+ * Revision 1.2  2002/12/20 21:30:06Z  ms10234
+ * Changed function declaration to take const char *'s
  * Revision 1.1  1999/05/28 01:40:22Z  TD10216
  * Initial revision
  * Revision 1.2  1998/08/27 22:04:52  TD07711
@@ -20,34 +22,25 @@
 #define _CHECKSELF_HEADER
 
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-// SPECIFICATION:  checkself() - generates a CRC over text and a portion of initialized data, and
-// verifies against the CRC in specified file.
-// inputs:
-//    argc, argv - passed through to checkself.  If "-checkself filename" is present then instead of doing
-//    the check and returning, checkself writes the calculated crc to /tmp/filename and exits(0).
-//    start - starting address of region to verify
-//    filename - file containing CRC to verify
-// outputs:
-//    returns 0 if CRC verifies, else -1
-// ERROR HANDLING:
-//    asserts that args are non null
-//    asserts that size of region to verify is > 0
-// USAGE:
-//    1. The position of checkself.o in the executable's link order determines what initialized data is
-//    covered by the check.  checkself.o must follow any other object/library files whose initialized
-//    data you want to cover.
-//    2. checkself() must be called before the executable has a chance to modify the initialized data
-//    covered.
-int checkself(int argc, const char** argv, const char* start_addr, const char* filename);
 
 #ifdef __cplusplus
-}
+extern "C" 
 #endif
+int checkself( char* startSymbol, char* endSymbol, const char* filename );
+    // PURPOSE:  verifies program loaded in memory has not changed.
+    // Verifies load integrity when run over text and initialized data segments following load.
+    // Verifies no text segment overwrites when run periodically thereafter.
+    // 
+    // checkself calculates a CRC over the data from startSymbol to endSymbol-1
+    //
+    // If filename exists, it reads the contained value as a previously saved CRC and verifies
+    // that it is the same as the one calculated.  
+    // Returns -1 if not validated, 0 if validated
+    //
+    // If filename does not exist, then filename is created and the calculated CRC is saved
+    // for validation on subsequent boots.
+    // Returns -1 if file not written, else 0
 
-#endif
+
+#endif // _CHECKSELF_HEADER
 

61255f64224615d34a088827c557e2e132878bef
Lindner, John <John.Lindner@Terumobct.com>
2003-03-27 14:27:44 -0800

    - allow disabling datalog levels by default [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14437]

--- include/datalog_levels.h
+++ include/datalog_levels.h
@@ -3,6 +3,8 @@
  *
  * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/include/rcs/datalog_levels.h 1.5 2003/04/25 17:25:02Z jl11312 Exp ms10234 $
  * $Log: datalog_levels.h $
+ * Revision 1.1  2003/03/27 15:48:00Z  jl11312
+ * Initial revision
  */
 
 #ifndef _DATALOG_LEVELS_INCLUDE
@@ -79,34 +81,35 @@ struct DataLog_InitTable
 	DataLog_Level * level;
 	DataLog_Handle * handle;
 	const char * name;
+	bool enabled;
 };
 
-DataLog_InitTable	initTable[] =
+static DataLog_InitTable	initTable[] =
 {
-	{ &log_level_message_system_timer_info, &log_handle_message_system_timer_info, "message_system_timer_info" },
-	{ &log_level_message_system_timer_error, &log_handle_message_system_timer_error, "message_system_timer_error" },
-	{ &log_level_router_info, &log_handle_router_info, "router_info" },
-	{ &log_level_router_error, &log_handle_router_error, "router_error" },
-	{ &log_level_gateway_info, &log_handle_gateway_info, "gateway_info" },
-	{ &log_level_gateway_error, &log_handle_gateway_error, "gateway_error" },
-	{ &log_level_message_spoof_info, &log_handle_message_spoof_info, "message_spoof_info" },
-	{ &log_level_message_spoof_error, &log_handle_message_spoof_error, "message_spoof_error" },
-	{ &log_level_message_system_info, &log_handle_message_system_info, "message_system_info" },
-	{ &log_level_message_system_error, &log_handle_message_system_error, "message_system_error" },
+	{ &log_level_message_system_timer_info, &log_handle_message_system_timer_info, "message_system_timer_info", true },
+	{ &log_level_message_system_timer_error, &log_handle_message_system_timer_error, "message_system_timer_error", true },
+	{ &log_level_router_info, &log_handle_router_info, "router_info", true },
+	{ &log_level_router_error, &log_handle_router_error, "router_error", true },
+	{ &log_level_gateway_info, &log_handle_gateway_info, "gateway_info", true },
+	{ &log_level_gateway_error, &log_handle_gateway_error, "gateway_error", true },
+	{ &log_level_message_spoof_info, &log_handle_message_spoof_info, "message_spoof_info", true },
+	{ &log_level_message_spoof_error, &log_handle_message_spoof_error, "message_spoof_error", true },
+	{ &log_level_message_system_info, &log_handle_message_system_info, "message_system_info", true },
+	{ &log_level_message_system_error, &log_handle_message_system_error, "message_system_error", true },
 
-	{ &log_level_datalog_info, &log_handle_datalog_info, "datalog_info" },
-	{ &log_level_datalog_error, &log_handle_datalog_error, "datalog_error" },
+	{ &log_level_datalog_info, &log_handle_datalog_info, "datalog_info", true },
+	{ &log_level_datalog_error, &log_handle_datalog_error, "datalog_error", true },
 
-	{ &log_level_vxworks_info, &log_handle_vxworks_info, "vxworks_info" },
-	{ &log_level_vxworks_error, &log_handle_vxworks_error, "vxworks_error" },
+	{ &log_level_vxworks_info, &log_handle_vxworks_info, "vxworks_info", true },
+	{ &log_level_vxworks_error, &log_handle_vxworks_error, "vxworks_error", true },
 
-	{ &log_level_cds_info, &log_handle_cds_info, "cds_info" },
-	{ &log_level_cds_error, &log_handle_cds_error, "cds_error" },
-	{ &log_level_cds_debug, &log_handle_cds_debug, "cds_debug" },
-	{ &log_level_cds_spoof_info, &log_handle_cds_spoof_info, "cds_spoof_info" },
-	{ &log_level_cds_spoof_error, &log_handle_cds_spoof_error, "cds_spoof_error" },
-	{ &log_level_socket_info, &log_handle_socket_info, "socket_info" },
-	{ &log_level_socket_error, &log_handle_socket_error, "socket_error" },
+	{ &log_level_cds_info, &log_handle_cds_info, "cds_info", true },
+	{ &log_level_cds_error, &log_handle_cds_error, "cds_error", true },
+	{ &log_level_cds_debug, &log_handle_cds_debug, "cds_debug", false },
+	{ &log_level_cds_spoof_info, &log_handle_cds_spoof_info, "cds_spoof_info", true },
+	{ &log_level_cds_spoof_error, &log_handle_cds_spoof_error, "cds_spoof_error", true },
+	{ &log_level_socket_info, &log_handle_socket_info, "socket_info", true },
+	{ &log_level_socket_error, &log_handle_socket_error, "socket_error", true },
 };
 
 #endif /* ifdef DATALOG_LEVELS_INIT_TABLE */

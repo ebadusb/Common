dc8b6a6d8de16102a03d18b6033431a8747d59d5
Lindner, John <John.Lindner@Terumobct.com>
2003-05-05 12:16:54 -0800

    - removed obsolete datalog level definitions [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14466]

--- include/common_datalog.h
+++ include/common_datalog.h
@@ -3,6 +3,8 @@
  *
  * $Header: K:/BCT_Development/vxWorks/Common/include/rcs/common_datalog.h 1.2 2003/03/27 15:47:34Z jl11312 Exp jl11312 $
  * $Log: common_datalog.h $
+ * Revision 1.2  2003/03/27 15:47:34Z  jl11312
+ * - added support for new data log levels
  */
 
 
@@ -10,12 +12,6 @@
 #define COMMON_DATALOG_H
 
 #include "datalog_levels.h"
-
-/*
- * Definitions below this line will go away when conversion to levels/handles in
- * datalog_levels.h is complete
- */
-
-#define LOG_DATASTORE           "log_datastore"
+#include "error.h"
 
 #endif /* ifndef COMMON_DATALOG_H */

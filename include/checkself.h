1434bd4650092d360706657057600df5fab499a5
td07711 <a@b>
2003-06-17 16:55:15 -0800

    changed checkself() prototype to accomodate non contiguous text/data segments. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14510]

--- include/checkself.h
+++ include/checkself.h
@@ -6,6 +6,8 @@
  * CHANGELOG:
  * $Header: I:/BCT_Development/vxWorks/Common/include/rcs/checkself.h 1.3 2003/02/26 23:03:05Z td07711 Exp td07711 $
  * $Log: checkself.h $
+ * Revision 1.3  2003/02/26 23:03:05Z  td07711
+ * mods for vxworks
  * Revision 1.2  2002/12/20 21:30:06Z  ms10234
  * Changed function declaration to take const char *'s
  * Revision 1.1  1999/05/28 01:40:22Z  TD10216
@@ -26,21 +28,27 @@
 #ifdef __cplusplus
 extern "C" 
 #endif
-int checkself( char* startSymbol, char* endSymbol, const char* filename );
-    // PURPOSE:  verifies program loaded in memory has not changed.
-    // Verifies load integrity when run over text and initialized data segments following load.
-    // Verifies no text segment overwrites when run periodically thereafter.
-    // 
-    // checkself calculates a CRC over the data from startSymbol to endSymbol-1
-    //
-    // If filename exists, it reads the contained value as a previously saved CRC and verifies
-    // that it is the same as the one calculated.  
-    // Returns -1 if not validated, 0 if validated
-    //
-    // If filename does not exist, then filename is created and the calculated CRC is saved
-    // for validation on subsequent boots.
-    // Returns -1 if file not written, else 0
 
+int checkself( char* textSymbol, char* dataSymbol, const char* outfile, const char* crcfile );
+
+    // PURPOSE:  verifies program loads into memory same way each time, i.e. it is bitwise the same
+    // as was validated.  Relies upon being loaded at same address each time and with all external
+    // references resolving identically as well, i.e. files defining the externs must also load
+    // at same address each time.
+    //
+    // textSymbol - is the symbol defining the start of the program's text segment - if a NULL ptr, then
+    // no checking of text segment is done.
+    // dataSymbol - is the symbol defining the start of the program's data segment - if a NULL ptr, then
+    // no checking of data segment is done.
+    // 
+    // outfile - is the full path name to the program's .out file.  Used to get size of text and data segments
+    // crcfile - is the full path name to an ASCII file containing the expected CRC to check
+    // 
+    // if crcfile does not exist, then checkself creates it with the calculated CRC
+    // returns -1 if error, 0 if file created OK.
+    //
+    // if crcfile does exist, then checkself reads the expected CRC and compares it to the calculated one.
+    // Returns -1 if expected CRC does not match, 0 if it does match
 
 #endif // _CHECKSELF_HEADER
 

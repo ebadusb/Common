9daadef7e1d7bf913e5858ad15b35d236ebbaf02
Lindner, John <John.Lindner@Terumobct.com>
2004-01-26 17:54:26 -0800

    - modifications for building under Tornado 2.2 [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14596]

--- create_common.bat
+++ create_common.bat
@@ -5,6 +5,8 @@ rem
 rem $Header: K:/BCT_Development/vxWorks/Common/rcs/create_common.bat 1.5 2003/02/04 16:00:30Z ms10234 Exp jl11312 $
 rem
 rem $Log: create_common.bat $
+rem Revision 1.5  2003/02/04 16:00:30Z  ms10234
+rem readded the latest_common_build_dir.mk file creation step
 rem Revision 1.4  2003/01/31 21:45:52Z  ms10234
 rem temporarily removed latest_common_build_dir file
 rem Revision 1.3  2003/01/31 19:17:18Z  ms10234
@@ -27,6 +29,32 @@ if not defined WIND_BASE goto find_tornado
 goto tornado_ok
 
 :tornado_ok
+grep -q "_VERSION *\"5.4" %WIND_BASE%\target\h\version.h
+if ERRORLEVEL 1 goto check_55
+
+echo vxWorks version 5.4.x
+attrib -r makefile.flags
+del makefile.flags
+copy makefile.flags_54 makefile.flags
+attrib +r makefile.flags
+goto start_build
+
+:check_55
+grep -q "_VERSION *\"5.5" %WIND_BASE%\target\h\version.h
+if ERRORLEVEL 1 goto unknown_version
+
+echo vxWorks version 5.5.x
+attrib -r makefile.flags
+del makefile.flags
+copy makefile.flags_55 makefile.flags
+attrib +r makefile.flags
+goto start_build
+
+:unknown_version
+echo Unknown vxWorks version
+goto end_of_script
+
+:start_build
 echo Start of common build
 date/t
 time/t

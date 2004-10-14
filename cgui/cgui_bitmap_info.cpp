b43d4172ef978bc5e00f72a544c731df70ef3bf0
cf10242 <a@b>
2004-10-14 12:26:53 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14773]

---
+++ cgui/cgui_bitmap_info.cpp
@@ -0,0 +1,100 @@
+/*
+* $Header: //BCTquad3/home/BCT_Development/vxWorks/Common/cgui/rcs/cgui_bitmap_info.cpp 1.3 2005/01/25 14:45:40 cf10242 Exp pn02526 $ 
+* This file implements the class that manages the bitmaps compiled into the application.
+* Each bitmap will have automatically generated an object of this type via the
+* build_bitmap_info file.
+*
+* $Log: cgui_bitmap_info.cpp $
+*
+*/
+
+#include "cgui_bitmap_info.h"
+#include "zlib.h"
+
+
+// constructor with a file name presented
+CGUIBitmapInfo::CGUIBitmapInfo(char * fileName) :	_myHeight (0),
+																	_myWidth (0),
+																	_loadCount (0),
+																	_loadState (UNLOADED),
+																	_myBitmap (NULL),
+																	_mySize(0),
+																	_myId(UGL_NULL_ID)
+{
+	// future capability - for now it does nothing but initialize some variables
+}
+
+// Uncompress the data loaded into the object at construction and create a UGL bitmap.
+void CGUIBitmapInfo::createDisplay (CGUIDisplay & dispObj)
+{
+	// if bitmap is already loaded, just increment loadCount and pass back 
+	// already created ugl id
+	if(_loadCount > 0)
+	{
+		_loadCount += 1;
+	}
+	else
+	{
+		// bitmap not loaded yet
+		unsigned long   bmpSize = _myHeight * _myWidth * sizeof(unsigned short);   // the USHORT is for RGB565 representation of color for bmp
+		unsigned char * bmpImage = new unsigned char[bmpSize];
+		UGL_DIB dib;
+
+		if(uncompress(bmpImage, &bmpSize, _myBitmap, _mySize) != Z_OK)
+		{
+			// log mesage to error log level and set a null ID
+			_myId = UGL_NULL_ID;
+		}
+		else
+		{
+			dib.height = _myHeight;
+			dib.width = _myWidth;
+			dib.stride = _myWidth;
+			dib.pImage = (void *)bmpImage;
+
+#if CPU==SIMNT
+
+			dib.clutSize = 0;
+			dib.pClut = NULL;
+
+			dib.colorFormat = UGL_DEVICE_COLOR;
+			dib.imageFormat = UGL_DIRECT;
+
+#else /* if CPU!=SIMNT */
+
+			dib.clutSize = 0;
+			dib.pClut = NULL;
+
+			dib.colorFormat = UGL_DEVICE_COLOR;
+			dib.imageFormat = UGL_DIRECT;
+
+#endif /* if CPU==SIMNT */
+
+			UGL_DEVICE_ID display = dispObj.display();
+			_myId = uglBitmapCreate(display, &dib, UGL_DIB_INIT_DATA, 0, UGL_DEFAULT_MEM);
+			delete[] bmpImage;
+
+			// get actual height and width
+	      UGL_SIZE width, height;
+		   uglBitmapSizeGet(_myId, &width, &height);
+			_myWidth = width;
+			_myHeight = height;
+		}
+	}
+}
+
+void CGUIBitmapInfo::removeDisplay(CGUIDisplay & dispObj)
+{
+	if (_loadCount > 0)
+	{
+		_loadCount -= 1;
+	}
+
+	if (_loadCount == 0 && _myId != UGL_NULL_ID)
+	{
+		UGL_DEVICE_ID display = dispObj.display();
+		uglBitmapDestroy(display, _myId);
+		_myId = UGL_NULL_ID;
+	}
+
+}
---
+++ cgui/cgui_bitmap_info.h
@@ -0,0 +1,91 @@
+/*
+* $Header: //BCTquad3/home/BCT_Development/vxWorks/Common/cgui/rcs/cgui_bitmap_info.h 1.5 2005/02/21 10:17:10 cf10242 Exp pn02526 $ 
+* This file defines the class that defines the bitmaps compiled into the application.
+* Each bitmap will have automatically generated an object of this type via the
+* build_bitmap_info file.
+*
+* $Log: cgui_bitmap_info.h $
+*
+*/
+#ifndef _CGUIBITMAPINFO_INCLUDE
+#define _CGUIBITMAPINFO_INCLUDE
+
+#include <vxworks.h>
+#include <stdio.h>
+
+#include <ugl/ugl.h>
+#include <ugl/uglwin.h>
+#include <ugl/uglinput.h>
+
+#include "cgui_graphics.h"
+
+class CGUIBitmapInfo
+{
+
+	public:
+
+		enum CGUIBitmapState
+		{
+			UNLOADED,
+			LOADED
+		};
+
+		typedef UGL_DDB_ID   CGUIBitmapId;  // create a CGUI type for the UGL ID associated with a bitmap
+
+		// CONSTRUCTOR - accepts a pointer to the input bitmap.  Expects that the data is in
+		// compressed format.
+		CGUIBitmapInfo (const unsigned char bmp_data[],
+							 unsigned short size,
+			             unsigned short height,
+							 unsigned short width) : 	_myHeight (height),
+																_myWidth (width),
+																_loadCount (0),
+																_loadState (UNLOADED)
+		{
+			_mySize = size;
+			_myBitmap = (unsigned char *)bmp_data; 
+		};
+
+		// CONSTRUCTOR - accepts a file path and name as input.  This constructor will open and
+		// read in the file to _myBitmap, set _myHeight and _myWidth as well.  THIS IS A FUTURE
+		// CAPABILITY AND IS NOT YET AVAILABLE!!!
+		CGUIBitmapInfo (char * fileName); 
+ 
+		// DESTRUCTOR - doesn't need to do anything at this point
+		virtual ~CGUIBitmapInfo()  {};
+
+		// getRawData -  return the pointer to the raw compressed data
+		unsigned char * getRawData (void)
+		{
+			return _myBitmap;
+		};
+
+		// getSize - return the width and height of the bitmap
+		void getSize(unsigned short & width, unsigned short & height)  
+		{ 
+			width = _myWidth;
+			height = _myHeight;
+		};
+
+		// createDisplay - decompress the bitmap loaded at construction and display the image
+		virtual void createDisplay (CGUIDisplay & display);
+
+		// removeDisplay - remove the image from the display
+		virtual void removeDisplay(CGUIDisplay & display);
+
+		// getId - return my CGUIBitmapId
+		CGUIBitmapId getId()   { return _myId;  };
+
+	private:
+		unsigned char * _myBitmap;
+		unsigned long _mySize;
+		unsigned short _myHeight,_myWidth;
+		CGUIBitmapState _loadState;
+		unsigned short _loadCount;
+		CGUIBitmapId _myId;
+};
+#endif
+
+
+
+

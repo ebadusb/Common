41217ff7276840918aec4a10c85f8f886f8e257b
rm10919 <a@b>
2004-11-18 21:34:09 -0800

    Naming conventions. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14804]

--- cgui/cgui_bitmap.cpp
+++ cgui/cgui_bitmap.cpp
@@ -4,6 +4,8 @@
 *  of the bitmap information and do the actual display of the graphics.
 *
 * $Log: cgui_bitmap.cpp $
+* Revision 1.1  2004/10/14 14:30:04Z  cf10242
+* Initial revision
  */
 
 #include <vxWorks.h>
@@ -11,26 +13,26 @@
 #include "cgui_bitmap_info.h"
 #include "cgui_window.h"
 
-CGUIBitmap::CGUIBitmap(CGUIDisplay & display, const CGUIRegion region, CGUIBitmapInfo & bitmapObj, bool realize)
-: CGUIWindowObject(display, region),	// initialize the parent
-	_display(display),							// set display reference
-	_bitmapObj (bitmapObj),					// set the bitmap data object
-	_region (region)							// initialize the region
+CGUIBitmap::CGUIBitmap(CGUIDisplay & display, const CGUIRegion region, CGUIBitmapInfo & bitmapObject, bool realize)
+: CGUIWindowObject(display, region),   // initialize the parent
+_display(display),               // set display reference
+_bitmapObject (bitmapObject),    // set the bitmap data object
+_region (region)              // initialize the region
 {
-	if(realize)
-		setBitmap();
+   if (realize)
+      setBitmap();
 }
 
 CGUIBitmap::~CGUIBitmap()
 {
-	unsigned short width, height;
+   unsigned short width, height;
    if (_owner)
    {
       _owner->deleteObject(this);
       _owner = NULL;
    }
 
-   _bitmapObj.removeDisplay(_display);
+   _bitmapObject.removeDisplay(_display);
 }
 
 void CGUIBitmap::setRegion(const CGUIRegion & newRegion)
@@ -43,12 +45,11 @@ void CGUIBitmap::setRegion(const CGUIRegion & newRegion)
 
 void CGUIBitmap::setBitmap()
 {
-
-	// tell bitmap object to display the image
-	_bitmapObj.createDisplay(_display);
+   // tell bitmap object to display the image
+   _bitmapObject.createDisplay(_display);
 
    CGUIRegion newRegion = _region;
-	_bitmapObj.getSize(_width, _height);
+   _bitmapObject.getSize(_width, _height);
    newRegion.width = _width;
    newRegion.height = _height;
    CGUIWindowObject::setRegion(newRegion);
@@ -65,19 +66,64 @@ void CGUIBitmap::setBitmap()
    }
 }
 
+void CGUIBitmap::setBitmap(CGUIBitmapInfo & bitmapObject)
+{   
+   if (_bitmapObject.getId() != 0)
+   {
+      //_display.unloadBitmap(_bitmapObject);
+      unloadBitmap();
+   }
+
+   _bitmapObject = bitmapObject;
+   setBitmap();
+
+//   if (_bitmapObject != NULL)
+//   {
+      //_uglId = _display.loadBitmap(_bitmapObject);
+
+//      UGL_SIZE width, height;
+      //uglBitmapSizeGet(_uglId, &width, &height);
+//      _bitmapObject.getSize(&width, &height);
+//      _width = width;
+ //     _height = height;
+//   }
+//   else
+//   {
+      //_uglId = UGL_NULL_ID;
+//      _width = 0;
+//      _height = 0;
+//   }
+
+//   CGUIRegion newRegion = _region;
+//   newRegion.width = _width;
+//   newRegion.height = _height;
+//   CGUIWindowObject::setRegion(newRegion);
+
+   //
+   // The explicit call to invalidate the bitmap's region is needed
+   // in case we changed to an identically sized bitmap (if the old and
+   // new regions are the same, OSWindowObject::set_region simply returns
+   // without forcing a redraw).
+   //
+   if (_owner)
+   {
+      _owner->invalidateObjectRegion(this);
+   }
+}
+
 void CGUIBitmap::unloadBitmap()
 {
-	
-	_bitmapObj.removeDisplay(_display);
 
-	unsigned short height, width;
-	CGUIRegion newRegion = _region;
-	_bitmapObj.getSize(width, height);
+   _bitmapObject.removeDisplay(_display);
+
+   unsigned short height, width;
+   CGUIRegion newRegion = _region;
+   _bitmapObject.getSize(width, height);
    newRegion.width = width;
    newRegion.height = height;
    CGUIWindowObject::setRegion(newRegion);
 
-	//
+   //
    // The explicit call to invalidate the bitmap's region is needed
    // in case we changed to an identically sized bitmap (if the old and
    // new regions are the same, CGUIWindowObject::set_region simply returns
@@ -91,7 +137,7 @@ void CGUIBitmap::unloadBitmap()
 
 void CGUIBitmap::draw(UGL_GC_ID gc)
 {
-	CGUIBitmapId uglId = _bitmapObj.getId();
+   CGUIBitmapId uglId = _bitmapObject.getId();
    if (uglId != UGL_NULL_ID)
    {
       uglBitmapBlt(gc, uglId, 0, 0, _width-1, _height-1, UGL_DEFAULT_ID, _region.x, _region.y);
--- cgui/cgui_bitmap.h
+++ cgui/cgui_bitmap.h
@@ -6,6 +6,8 @@
  *
  * $Header: H:/BCT_Development/vxWorks/Common/cgui/rcs/cgui_bitmap.h 1.4 2004/11/18 22:34:09Z rm10919 Exp wms10235 $
  * $Log: cgui_bitmap.h $
+ * Revision 1.3  2004/10/14 14:27:39Z  cf10242
+ * First working bitmap class
  * Revision 1.2  2004/09/30 17:00:52Z  cf10242
  * Correct for initial make to work
  * Revision 1.1  2004/09/20 18:18:12Z  rm10919
@@ -28,22 +30,23 @@
 class CGUIBitmap : public CGUIWindowObject
 {
 public:
-	// CONSTRUCTOR - must provide a display reference, region, and bitmap data reference.  Can elect
-	// to realize the graphics on the display or not.  Note that for bitmaps the region only defines the
-	// start location (x,y) of the bitmap.  The height and width are set by the size of the bitmap itself.
-   CGUIBitmap(CGUIDisplay & display, const CGUIRegion region, CGUIBitmapInfo & bitmapObj,  bool realize=true);
-	//DESTRUCTIOR
-	virtual ~CGUIBitmap(void);
+   // CONSTRUCTOR - must provide a display reference, region, and bitmap data reference.  Can elect
+   // to realize the graphics on the display or not.  Note that for bitmaps the region only defines the
+   // start location (x,y) of the bitmap.  The height and width are set by the size of the bitmap itself.
+   CGUIBitmap(CGUIDisplay & display, const CGUIRegion region, CGUIBitmapInfo & bitmapObject,  bool realize=true);
+   //DESTRUCTIOR
+   virtual ~CGUIBitmap(void);
 
-	// setRegion - override of the parent setRegion to maintain a fixed height and width
-	//  for the bitmap since that can not change (although location of the bitmap may change).
+   // setRegion - override of the parent setRegion to maintain a fixed height and width
+   //  for the bitmap since that can not change (although location of the bitmap may change).
    virtual void setRegion(const CGUIRegion & newRegion);
 
-	// setBitmap - realize the bitmap at the currently set region
+   // setBitmap - realize the bitmap at the currently set region
    void setBitmap(void);
+   void setBitmap(CGUIBitmapInfo & bitmapObject);
 
-	// unloadBitmap - remove the bitmap from the display at the currently set region
-	void unloadBitmap (void);
+   // unloadBitmap - remove the bitmap from the display at the currently set region
+   void unloadBitmap (void);
 
 protected:
    virtual void draw(UGL_GC_ID gc);
@@ -52,11 +55,11 @@ private:
    virtual bool clipSiblings(void) const { return true;}
 
 private:
-	CGUIRegion _region;
-	unsigned short _width;
+   CGUIRegion _region;
+   unsigned short _width;
    unsigned short _height;
-	CGUIBitmapInfo & _bitmapObj;
-	CGUIDisplay & _display;
+   CGUIBitmapInfo & _bitmapObject;
+   CGUIDisplay & _display;
 };
 
 #endif /* #ifndef _CGUI_BITMAP_INCLUDE */

bd2faefa70ed6f9d8aea76babcd63f12ecda3236
Lindner, John <John.Lindner@Terumobct.com>
2005-05-11 13:15:10 -0800

    - corrected archive types [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14876]

---
+++ tools/config_parse/config_parse.h
@@ -0,0 +1,16 @@
+// $Header: K:/BCT_Development/Taos/tools/config_parse/rcs/config_parse.h 1.1 2004/06/24 03:23:48Z jl11312 Exp jl11312 $
+//
+// Configuration file parse tool
+//
+// $Log: config_parse.h $
+// Revision 1.1  2004/06/24 03:23:48Z  jl11312
+// Initial revision
+//
+
+#ifndef _CONFIG_PARSE_INCLUDE
+#define _CONFIG_PARSE_INCLUDE
+
+#include <vector>
+#include "config_file.h"
+
+#endif /* ifndef _CONFIG_PARSE_INCLUDE */

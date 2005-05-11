a9c19ebcbfd4e2597d3d56adf8c18441e9a9abf5
Lindner, John <John.Lindner@Terumobct.com>
2005-05-11 13:15:08 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 14873]

--- tools/config_parse/parameter.h
+++ tools/config_parse/parameter.h
@@ -1,4 +1,4 @@
-// $Header: K:/BCT_Development/vxWorks/Common/tools/config_parse/rcs/parameter.h 1.2 2005/05/11 15:15:08Z jl11312 Exp jl11312 $
+// $Header: K:/BCT_Development/vxWorks/Common/tools/config_parse/rcs/parameter.h 1.1 2005/05/11 15:15:08Z jl11312 Exp jl11312 $
 //
 // Configuration file parameter classes
 //
--- tools/config_parse/section.h
+++ tools/config_parse/section.h
@@ -1,4 +1,4 @@
-// $Header: K:/BCT_Development/Taos/tools/config_parse/rcs/section.h 1.1 2005/01/13 20:25:54Z jl11312 Exp jl11312 $
+// $Header: K:/BCT_Development/vxWorks/Common/tools/config_parse/rcs/section.h 1.1 2005/05/11 15:15:08Z jl11312 Exp jl11312 $
 //
 // Configuration file section class
 //

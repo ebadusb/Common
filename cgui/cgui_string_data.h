04c58d2b614f2dbb187247d7593d0562f8c9103c
pn02526 <a@b>
2006-11-28 19:42:55 -0800

    Use CGUIStringInfo class to read string.info files. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16114]

--- cgui/cgui_string_data.h
+++ cgui/cgui_string_data.h
@@ -3,6 +3,8 @@
  *
  * $Header: K:/BCT_Development/vxWorks/Common/cgui/rcs/cgui_string_data.h 1.6 2007/05/03 16:19:14Z jl11312 Exp wms10235 $
  * $Log: cgui_string_data.h $
+ * Revision 1.2  2006/07/25 15:42:37  cf10242
+ * IT 54: decode UTF8 into unicode
  * Revision 1.1  2005/04/27 13:40:46Z  rm10919
  * Initial revision
  *
@@ -27,14 +29,13 @@ public:
    void addTextItem(const char * name, CGUITextItem * textItem);
    void deleteTextItem (const char * name);
 
-   bool readDatabaseFile (const char * filename, CGUIFontId * fontId, LanguageId languageId = currentLanguage);
+   void readDatabaseFile (const char * filename, CGUIFontId * fontId, LanguageId languageId = currentLanguage);
    bool readDatabaseItem (CGUITextItem * LanguageName); //, Language language[0]);
 
+protected:
    map<string, CGUITextItem *> _textMap;
 
-protected:
-
 private:
-	StringChar UTF8ToUnicode(StringChar utf8Char, int line);
+
 };
 #endif //_CGUI_STRING_DATA_INCLUDE

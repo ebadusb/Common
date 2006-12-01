51b07e4038d0eb520ec1a359d29dd700299a3f71
jmedusb <a@b>
2006-11-30 16:54:13 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16128]

---
+++ datalog/unit_test/datalog_reserved_stream.cpp
@@ -0,0 +1,17 @@
+/*
+ * $Header: Z:/vxWorks/Common/datalog/rcs/datalog_reserved_stream.cpp 1.2 2006/10/23 20:23:27Z jmedusb Exp $
+ * Defines constants used for the DataLogReserved message type
+ * RESERVED_HEADER is the text tag name for reserved messages
+ * DEFAULT_NAME is used when no name is passed into a DataLogReserved call
+ *
+ * $Log: datalog_reserved_stream.cpp $
+ * Revision 1.2  2006/10/23 20:23:27Z  jmedusb
+ * Added standard header.
+ */
+
+
+#include "vxworks.h"
+#include "datalog_reserved_stream.h"
+
+const char * DataLogRes::RESERVED_HEADER = "DATALOG_RESERVED";
+const char * DataLogRes::DEFAULT_NAME    = "DEFAULT_NAME";

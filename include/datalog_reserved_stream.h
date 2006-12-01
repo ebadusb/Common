7a5b18291ffa55e394030d8e97cd5b5ca024a86a
Lindner, John <John.Lindner@Terumobct.com>
2006-11-30 16:39:36 -0800

    - back out rev 1.4 which caused the common build to break [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16125]

--- include/datalog_reserved_stream.h
+++ include/datalog_reserved_stream.h
@@ -1,5 +1,5 @@
 /*
- * $Header: K:/BCT_Development/vxWorks/Common/include/rcs/datalog_reserved_stream.h 1.4 2006/11/30 17:26:33Z jmedusb Exp jl11312 $ 
+ * $Header: Z:/vxWorks/Common/include/rcs/datalog_reserved_stream.h 1.5 2006/11/30 17:39:36Z jl11312 Exp jmedusb $ 
  *
  *  This defined the DataLogReserved class constructor and stream
  *  manipulator template functions, as well as the dec/hex and
@@ -29,38 +29,30 @@
 #include "datalog.h"
 
 //This defines the function pointer that takes only one argument, the stream reference
-class DataLogRes;
-typedef DataLogRes & (* DataLogReservedManip)(DataLogRes &);
+class DataLogReserved;
+typedef DataLogReserved & (* DataLogReservedManip)(DataLogReserved &);
 
-#define DataLogReserved(name, level) DataLogRes(name, level, __FILE__, __LINE__)
 
-class DataLogRes
+class DataLogReserved
 {
 	public:
 		template < class T >
-			friend DataLogRes & manipTaggedItem(DataLogRes & stream, const char * tagName, T tagValue);
+			friend DataLogReserved & manipTaggedItem(DataLogReserved & stream, const char * tagName, T tagValue);
 		//Manipulator for settting precision
-		friend DataLogRes & manipPrecision(DataLogRes & stream, int tagValue);
-		friend DataLogRes & endmsg(DataLogRes & stream);
-		friend DataLogRes & hex(DataLogRes & stream);
-		friend DataLogRes & dec(DataLogRes & stream);
+		friend DataLogReserved & manipPrecision(DataLogReserved & stream, int tagValue);
+		friend DataLogReserved & endmsg(DataLogReserved & stream);
+		friend DataLogReserved & hex(DataLogReserved & stream);
+		friend DataLogReserved & dec(DataLogReserved & stream);
 
-        virtual ~DataLogRes() {}
+        virtual ~DataLogReserved() {}
         //Keep track of the log level this way we can reference the parent stream
-		//Passes the line and file info of the calling location
-        DataLogRes(const char * messageName, DataLog_Level & level, const int file, const int line) : _logLevel(level)
+        DataLogReserved(const char * messageName, DataLog_Level & level) : _logLevel(level)
         {
-            level(file, line) << RESERVED_HEADER << (messageName == NULL ? DEFAULT_NAME : messageName);
-		}
-
-		//Constructor uses the line and file of this header file
-		DataLogRes(const char * messageName, DataLog_Level & level) : _logLevel(level)
-        {
-            DataLogRes(messageName, level, __FILE__, __LINE__);
+            DataLog(level) << RESERVED_HEADER << (messageName == NULL ? DEFAULT_NAME : messageName);
 		}
 
         //This is so we can use endmsg from from DataLog_Stream
-        DataLogRes & operator << (DataLogReservedManip func)
+        DataLogReserved & operator << (DataLogReservedManip func)
 		{ 
 			return (*func)(*this);
 		}
@@ -74,20 +66,20 @@ class DataLogRes
 
 //This is the manipulator function that does the work
 template < class T >
-inline DataLogRes & manipTaggedItem(DataLogRes & stream, const char * tagName, T tagValue)
+inline DataLogReserved & manipTaggedItem(DataLogReserved & stream, const char * tagName, T tagValue)
 {
 	DataLog(stream._logLevel) << tagName << tagValue;
 	return stream;
 }
 
 //This is the manipulator function that does the work
-inline DataLogRes & manipPrecision(DataLogRes & stream, int tagValue)
+inline DataLogReserved & manipPrecision(DataLogReserved & stream, int tagValue)
 {
 	DataLog(stream._logLevel).precision(tagValue);
 	return stream;
 }
 
-inline DataLogRes & endmsg(DataLogRes & stream)
+inline DataLogReserved & endmsg(DataLogReserved & stream)
 {
 	DataLog(stream._logLevel) << endmsg;
 	return stream;
@@ -101,7 +93,7 @@ inline DataLogReserved & hex(DataLogReserved & stream)
 }
 
 //Decimal precision for integral values
-inline DataLogRes & dec(DataLogRes & stream)
+inline DataLogReserved & dec(DataLogReserved & stream)
 {
 	DataLog(stream._logLevel) << dec;
 	return stream;
@@ -114,20 +106,20 @@ class DataLogReservedIManip
 	S _value;
 	const char * _name;
 	//TaggedItem form for manipulator take a tag name and S tagValue
-	DataLogRes & (* _func)(DataLogRes & stream, const char * name, S value);
+	DataLogReserved & (* _func)(DataLogReserved & stream, const char * name, S value);
 
 public:
 	//Pointer to Stream Manipulator Function for TaggedItem
 	DataLogReservedIManip(	
-		DataLogRes & (* func)(DataLogRes & stream, const char * name, S value),
+		DataLogReserved & (* func)(DataLogReserved & stream, const char * name, S value),
 		const char * name,			//Tag Name
 		S value)					//Tag Value
 	  : _func(func), _value(value), _name(name) { }
 
     template < class T >
-		friend DataLogRes & operator << (DataLogRes stream, const DataLogReservedIManip<T> & manip);
+		friend DataLogReserved & operator << (DataLogReserved stream, const DataLogReservedIManip<T> & manip);
 	template < class T >
-		friend DataLogRes & manipTaggedItem(DataLogRes & stream, const char * tagName, T tagValue);
+		friend DataLogReserved & manipTaggedItem(DataLogReserved & stream, const char * tagName, T tagValue);
 };
 
 //This is the manipulator class for the Precision manipulator
@@ -135,17 +127,17 @@ class DataLogReservedIManipP
 {
 	int _value;
     //Precision form for manipulator takes an S precision value
-	DataLogRes & (* _pFunc)(DataLogRes & stream, int value);
+	DataLogReserved & (* _pFunc)(DataLogReserved & stream, int value);
 
 public:
 	//Pointer to Stream Manipulator Function for Precision
 	DataLogReservedIManipP(	
-		DataLogRes & (* func)(DataLogRes & stream, int value),
+		DataLogReserved & (* func)(DataLogReserved & stream, int value),
         int value)					//Tag Value
 	  : _pFunc(func), _value(value) { }
 
-	friend DataLogRes & operator << (DataLogRes stream, const DataLogReservedIManipP & manip);
-	friend DataLogRes & manipPrecision(DataLogRes & stream, int tagValue);
+	friend DataLogReserved & operator << (DataLogReserved stream, const DataLogReservedIManipP & manip);
+	friend DataLogReserved & manipPrecision(DataLogReserved & stream, int tagValue);
 };
 
 //This calls the taggedItem manipulator
@@ -162,12 +154,12 @@ inline DataLogReservedIManipP setPrecision(int tagValue)
 }
 
 template < class S >
-inline DataLogRes & operator << (DataLogRes stream, const DataLogReservedIManip<S> & manip)
+inline DataLogReserved & operator << (DataLogReserved stream, const DataLogReservedIManip<S> & manip)
 {
 	return (manip._func)(stream, manip._name, manip._value);
 }
 
-inline DataLogRes & operator << (DataLogRes stream, const DataLogReservedIManipP & manip)
+inline DataLogReserved & operator << (DataLogReserved stream, const DataLogReservedIManipP & manip)
 {
 	return (manip._pFunc)(stream, manip._value);
 }

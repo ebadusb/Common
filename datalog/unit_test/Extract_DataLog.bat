5da9bde5d828bc7da9e6b7a8303616ea2b6a1de5
jmedusb <a@b>
2006-11-30 16:25:21 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16123]

---
+++ datalog/unit_test/Extract_DataLog.bat
@@ -0,0 +1,2 @@
+call log_convert.exe 1A00001_2006MMDD_000_000001.dlog > DataLogReserved_Test_001.txt
+call log_convert.exe 1A00001_20061030_000_000001.dlog > DataLogReserved_20061030.txt
---
+++ datalog/unit_test/make_test_debug.bat
@@ -0,0 +1,1 @@
+make -f makefile_reserved.vx BUILD_TYPE=DEBUG CPU=SIMNT

353232df35cb081f1532cb69f688248ffcd2032e
jmedusb <a@b>
2006-11-30 16:17:55 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16120]

---
+++ datalog/unit_test/alarm_header_text_message_res.cpp
@@ -0,0 +1,4 @@
+#include <vxWorks.h>
+#include "alarm_header_text_message_res.h"
+
+const AlarmHeaderTextMessageRes::AlarmHeaderText AlarmHeaderTextMessageRes::alarmHeaderText = { "ALARM_HEADER_TEXT", "TEXT_ID", "TEXT_STRING" };
---
+++ datalog/unit_test/alarm_header_text_message_res.h
@@ -0,0 +1,17 @@
+#ifndef _ALARM_HEADER_TEXT_MESSAGE_RES_
+#define _ALARM_HEADER_TEXT_MESSAGE_RES_
+
+namespace AlarmHeaderTextMessageRes
+{
+	struct AlarmHeaderText
+	{
+		const char * name;
+		const char * textID;
+		const char * textString;
+	};
+
+	extern const AlarmHeaderText alarmHeaderText;
+
+}
+
+#endif _ALARM_HEADER_TEXT_MESSAGE_RES_

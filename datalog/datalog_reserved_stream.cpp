49457b0cfd20de3576045e89d6e808a55e9f2c3a
MS10234 <a@b>
2006-12-28 18:28:09 -0800

    - add ability to reuse a non-terminated reserved message stream without putting the header on each line. [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16175]

--- datalog/datalog_reserved_stream.cpp
+++ datalog/datalog_reserved_stream.cpp
@@ -1,10 +1,12 @@
 /*
- * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/datalog/rcs/datalog_reserved_stream.cpp 1.3 2006/11/30 17:59:32Z jmedusb Exp MS10234 $
+ * $Header: //bctquad3/home/BCT_Development/vxWorks/Common/datalog/rcs/datalog_reserved_stream.cpp 1.3 2006/11/30 17:59:32Z jmedusb Exp $
  * Defines constants used for the DataLogReserved message type
  * RESERVED_HEADER is the text tag name for reserved messages
  * DEFAULT_NAME is used when no name is passed into a DataLogReserved call
  *
  * $Log: datalog_reserved_stream.cpp $
+ * Revision 1.3  2006/11/30 17:59:32Z  jmedusb
+ * Updates the ReservedStream def to the correct Name IT#70
  * Revision 1.1  2006/11/30 17:54:13Z  jmedusb
  * Initial revision
  * Revision 1.2  2006/10/23 20:23:27Z  jmedusb
@@ -17,3 +19,6 @@
 
 const char * DataLogRes::RESERVED_HEADER = "DATALOG_RESERVED";
 const char * DataLogRes::DEFAULT_NAME    = "DEFAULT_NAME";
+
+map<unsigned long,set<DataLog_Level*> > DataLogRes::_levelsInProcess;
+

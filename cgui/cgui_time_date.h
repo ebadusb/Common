3ae65288a1067c258c71afd4d0264e01c72f7576
pn02526 <a@b>
2007-03-22 13:19:22 -0800

    Add two CGUITextItem pointers for AM and PM strings (suffixes for 12-hour time strings) to the calling sequence of the constructor, plus functions to get and set them post-construction. The two new constructor arguments are transparent to existing code. If NULL in the constructor or the set function, the class does not attempt to append anything to twelve-hour time strings. To internationalize this, the string handling within the class has been changed to use the wide character type StringChar used internally in the CGUIText and CGUITextItem classes. The stringCharxxxx functions have been added for this. Change the date separator character must be changed to hyphen for internationalizing date strings.
    --- Added comments ---  pn02526 [2007/03/22 18:35:30Z]
    IT 77.
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16445]

--- cgui/cgui_time_date.h
+++ cgui/cgui_time_date.h
@@ -1,8 +1,10 @@
 /*
  *	Copyright (c) 2004 by Gambro BCT, Inc.  All rights reserved.
  *
- * $Header: //BCTquad3/home/BCT_Development/vxWorks/Common/cgui/rcs/cgui_time_date.h 1.4 2005/03/14 18:21:37 rm10919 Exp pn02526 $
+ * $Header: K:/BCT_Development/vxWorks/Common/cgui/rcs/cgui_time_date.h 1.6 2007/03/22 20:57:49Z pn02526 Exp wms10235 $
  * $Log: cgui_time_date.h $
+ * Revision 1.4  2005/03/14 18:21:37  rm10919
+ * Change CGUIText to not add object to window object list of parent in constructor.
  * Revision 1.3  2005/01/28 23:52:18Z  rm10919
  * CGUITextItem class changed and put into own file.
  * Revision 1.2  2004/11/03 17:43:55Z  rm10919
@@ -24,20 +26,22 @@ class CGUITimeDate : public CGUIText
 {
 public:
    enum DisplayTypeAttributes
-   { TIME_DATE = 0x0000, DATE_ONLY = 0x0001, TIME_ONLY = 0x0002 };
+   { DISPLAY_TYPE_ATTRIBUTES_MASK=0x000f, TIME_DATE = 0x0000, DATE_ONLY = 0x0001, TIME_ONLY = 0x0002 };
 
    enum ClockAttributes
-   { TWELVE_HOUR = 0x0000, TWENTYFOUR_HOUR = 0x0010 };
+   { CLOCK_ATTRIBUTES_MASK=0x00f0, TWELVE_HOUR = 0x0000, TWENTYFOUR_HOUR = 0x0010 };
 
    enum DateAttributes
-   { MM_DD_YY = 0x0000, DD_MM_YY = 0x0100, YY_MM_DD = 0x0200 };
+   { DATE_ATTRIBUTES_MASK=0x0f00, MM_DD_YY = 0x0000, DD_MM_YY = 0x0100, YY_MM_DD = 0x0200 };
 
    enum LineOrderAttributes
-   { ONE_LINE_DATE_TIME = 0x0000, ONE_LINE_TIME_DATE = 0x1000, TWO_LINE_DATE_TIME = 0x2000, TWO_LINE_TIME_DATE = 0x3000 };
+   { LINE_ORDER_ATTRIBUTES_MASK=0xf000, ONE_LINE_DATE_TIME = 0x0000, ONE_LINE_TIME_DATE = 0x1000, TWO_LINE_DATE_TIME = 0x2000, TWO_LINE_TIME_DATE = 0x3000 };
 
    CGUITimeDate(CGUIDisplay & display,
                 StylingRecord * stylingRecord, unsigned int displayAttributes = 0,
-                CGUITextItem * timeDateTextItem = NULL);
+                CGUITextItem * timeDateTextItem = NULL,
+                CGUITextItem * amTextItem = NULL,
+                CGUITextItem * pmTextItem = NULL);
 
    virtual ~CGUITimeDate(void);
 
@@ -46,11 +50,24 @@ public:
    void setDisplayAttributes(unsigned int displayAttributes);
    unsigned int getDisplayAttributes(void) {return _displayAttributes;}
 
+   void setAMPMText(CGUITextItem * amTextItem = NULL, CGUITextItem * pmTextItem = NULL) { _amTextItem=amTextItem; _pmTextItem=pmTextItem; update(); };
+   void getAMPMText(CGUITextItem ** amTextItem = NULL, CGUITextItem ** pmTextItem = NULL) { *amTextItem=_amTextItem; *pmTextItem=_pmTextItem; update(); };
+
    void update();
 
 private:
    
    unsigned int    _displayAttributes;
+
+    CGUITextItem * _amTextItem;
+    CGUITextItem * _pmTextItem;
+
+    StringChar * stringChar( const char * string );
+    size_t stringCharLen( const StringChar * s );
+    inline size_t stringCharAllocation(const size_t len);
+    inline size_t stringCharAllocation(const StringChar * s);
+    void stringCharCpy( StringChar ** ps1, const StringChar * s2 );
+    void stringCharCat( StringChar ** ps1, const StringChar * s2 );
 };
 
 #endif /* ifndef _CGUI_TIME_DATE_INCLUDE */

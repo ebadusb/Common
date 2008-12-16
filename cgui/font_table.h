aa7c995d61fb768b94185ff95b630d9fbdd4d0b0
rm10919 <a@b>
2008-12-16 05:06:58 -0800

    Initial revision [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 16742]

---
+++ cgui/font_table.h
@@ -0,0 +1,21 @@
+/*
+ * $Header: J:/BCT_Development/Taos/base/gui/rcs/font_table.h 1.1 2005/08/26 12:17:44Z jl11312 Exp rm10919 $
+ *
+ * 	Header file for reference to function defined in the project.
+ *
+ * $Log:$
+ * 
+ * 
+ */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* ifdef __cplusplus */
+
+int getFontIndex( const char *fontName );
+
+#ifdef __cplusplus
+}; // extern "C"
+#endif /* ifdef __cplusplus */
+

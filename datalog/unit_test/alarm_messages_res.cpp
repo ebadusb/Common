2514c5aa59ce274580356166d1f239b1aca0144e
adalusb <a@b>
2010-08-16 07:41:37 -0800

    Removed obsolete files
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 17196]

--- datalog/unit_test/alarm_messages_res.cpp
+++
@@ -1,7 +0,0 @@
-#include <vxWorks.h>
-#include "alarm_messages_res.h"
-
-const AlarmMessagesReserved::DisplayingAlarm AlarmMessagesReserved::displayingAlarm = { "DISPLAYING_ALARM", "logName", "eventID", "nodeID", "moduleLevel", "alarmID" };
-const AlarmMessagesReserved::AlarmHeaderText AlarmMessagesReserved::alarmHeaderText = { "ALARM_HEADER_TEXT", "textID", "textString" };
-const AlarmMessagesReserved::AlarmBodyText AlarmMessagesReserved::alarmBodyText = { "ALARM_BODY_TEXT", "textID", "textString" };
-const AlarmMessagesReserved::AlarmConstraint AlarmMessagesReserved::alarmConstraint = { "ALARM_CONSTRAINT", "constraintName", "referenceCount" };

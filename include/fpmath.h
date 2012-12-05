714aac37bca80d379308d2074f10bcae74cf3b66
Scott, Mark <Mark.Scott@terumobct.com>
2012-12-05 11:49:43 -0800

    CommonIT137 - change compare functions to use fpCompare, rename functions to fpFloor, fpCeiling, and fpRound to avoid name clash with common math.h functions.
    Jobs: job003015
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 50760]

--- include/fpmath.h
+++ include/fpmath.h
@@ -15,76 +15,78 @@
 namespace FPMath
 {
 
-//
-// COMPARE TWO FLOATING POINT VALUES --
-// The first parameter will be compared to the second parameter with a scaled 
-//    epsilon value (10 * FLT_EPSILON * first parmeter). 
-// The function will return:
-//   -1 - the first parameter is less than the second
-//    0 - the parameter are equal to each other, within the epsilon value
-//    1 - the first parameter is greater than the second
-//
-int fpCompare(const double lhs, const double rhs);
+	//
+	// COMPARE TWO FLOATING POINT VALUES --
+	// The first parameter will be compared to the second parameter with a scaled 
+	//    epsilon value (10 * FLT_EPSILON * first parmeter). 
 
-//
-// Same as above, but lets the user of the function dictate what epsilon is
-//    Note: epsilon will be set = (10 * FLT_EPSILON * first parameter)
-//    if epsilon is < (10 * FLT_EPSILON * first parameter)
-//
-int fpCompare(const double lhs, const double rhs, const double epsilon);
+	//    Note: If using the form that allows a passed in epsion value, thought 
+	//          should be given to choosing an epsilon value.  Here is 
+	//          a link to use when determining the epsilon value:
+	//
+	//			http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
+	//
+	// The function will return:
+	//   -1 - the first parameter is less than the second
+	//    0 - the parameter are equal to each other, within the epsilon value
+	//    1 - the first parameter is greater than the second
+	//
+	int fpCompare(const double lhs, const double rhs);
+	int fpCompare(const double lhs, const double rhs, const double epsilon);
 
-// 
-// Returns the average of a linked list of doubles
-//
-double fpAvg(std::list<double> &listToAvg);
+	// 
+	// Returns the average of a linked list of doubles
+	//
+	double fpAvg(std::list<double> &listToAvg);
 
-//
-// Returns the standard deviation of a linked list of doubles
-// 
-double fpStdDev(std::list<double> &list);
+	//
+	// Returns the standard deviation of a linked list of doubles
+	// 
+	double fpStdDev(std::list<double> &list);
 
-//
-// Returns the value rounded to the given precision. Use 0 to negative precision for integers.
-//
-template<class T>
-T fpPreciseRound(T value, int precision)
-{
-	T rounded_value;
-	rounded_value = static_cast<T>(floor(value * pow(10.0, precision) + .5) / pow(10.0, precision));
-	return rounded_value;
-}
-/// \return true if the value in lhs is within epsilon of rhs.
-bool compareEqual(double lhs, double rhs, double epsilon);
-/// This version of the comparison implicitly uses DBL_EPSILON
-/// as a comparison value.
-bool compareEqual(double lhs, double rhs);
+	//
+	// Returns the value rounded to the given precision. Use 0 to negative precision for integers.
+	//
+	template<class T>
+	T fpPreciseRound(T value, int precision)
+	{
+		T rounded_value;
+		rounded_value = static_cast<T>(floor(value * pow(10.0, precision) + .5) / pow(10.0, precision));
+		return rounded_value;
+	}
 
-/// (lhs > rhs)
-bool compareGreater(double lhs, double rhs, double epsilon);
-bool compareGreater(double lhs, double rhs);
+	/// \return true if the value in lhs is within epsilon of rhs.
+	bool compareEqual(double lhs, double rhs, double epsilon);
+	/// This version of the comparison implicitly supplies an appropriate epsilon value
+	/// as a comparison value.
+	bool compareEqual(double lhs, double rhs);
 
-/// (lhs < rhs)
-bool compareLess(double lhs, double rhs, double epsilon);
-bool compareLess(double lhs, double rhs);
+	/// (lhs > rhs)
+	bool compareGreater(double lhs, double rhs, double epsilon);
+	bool compareGreater(double lhs, double rhs);
 
-/// (lhs >= rhs)
-bool compareGreaterOrEqual(double lhs, double rhs, double epsilon);
-bool compareGreaterOrEqual(double lhs, double rhs);
+	/// (lhs < rhs)
+	bool compareLess(double lhs, double rhs, double epsilon);
+	bool compareLess(double lhs, double rhs);
 
-bool compareLessOrEqual(double lhs, double rhs, double epsilon);
-bool compareLessOrEqual(double lhs, double rhs);
+	/// (lhs >= rhs)
+	bool compareGreaterOrEqual(double lhs, double rhs, double epsilon);
+	bool compareGreaterOrEqual(double lhs, double rhs);
 
-bool compareInRange(double value, double min, double max, double epsilon);
-bool compareInRange(double value, double min, double max);
+	bool compareLessOrEqual(double lhs, double rhs, double epsilon);
+	bool compareLessOrEqual(double lhs, double rhs);
 
-bool isNan(double value);
-bool isInf(double value);
+	bool compareInRange(double value, double min, double max, double epsilon);
+	bool compareInRange(double value, double min, double max);
 
-double floor(double value);
-double ceiling(double value);
-int round(double value);
+	bool isNan(double value);
+	bool isInf(double value);
 
-int getNumberOfIntegralDigits(double value);
+	double fpFloor(double value);
+	double fpCeiling(double value);
+	int fpRound(double value);
+
+	int getNumberOfIntegralDigits(double value);
 };
 
 #endif
--- utility/fpmath.cpp
+++ utility/fpmath.cpp
@@ -13,176 +13,175 @@
 
 int FPMath::fpCompare(const double lhs, const double rhs)
 {
-   double epsilon = 10 * FLT_EPSILON * ( lhs == 0 ? 1 : fabs(lhs) );
+	double absLHS = fabs(lhs);
+	double absRHS = fabs(rhs);
+	double scaleValue = ( absLHS > absRHS ? absLHS : absRHS );
+	double epsilon = 10 * FLT_EPSILON * ( scaleValue == 0 ? 1 : scaleValue );
 
-   return fpCompare(lhs,rhs,epsilon); 
+	return fpCompare(lhs,rhs,epsilon); 
 }
 
 int FPMath::fpCompare(const double lhs, const double rhs, const double epsilon)
 {
-   double minEpsilon = 10 * FLT_EPSILON * ( lhs == 0 ? 1 : fabs(lhs) );
+	int retVal;
+	if ( lhs > rhs + epsilon )	retVal = 1;
+	else if ( lhs < rhs - epsilon ) retVal = -1;
+	else retVal	= 0;
 
-   double realEpsilon = epsilon;
-   if (epsilon < minEpsilon) realEpsilon = minEpsilon;
-
-   int retVal;
-   if ( lhs > rhs + realEpsilon ) retVal = 1;
-   else if ( lhs < rhs - realEpsilon ) retVal = -1;
-   else retVal = 0;
-
-   return retVal;
+	return retVal;
 }
 
 double FPMath::fpAvg(std::list<double> &listToAvg)
 {
-   double runningAvg = 0.0;
+	double runningAvg = 0.0;
 
-   if (listToAvg.size() > 0)
-   {
-      std::list<double>::iterator iter = listToAvg.begin();
-      for (; iter != listToAvg.end(); ++iter)
-      {
-         runningAvg += *iter;
-      }
+	if ( listToAvg.size() > 0 )
+	{
+		std::list<double>::iterator iter = listToAvg.begin();
+		for ( ; iter != listToAvg.end(); ++iter )
+		{
+			runningAvg += *iter;
+		}
 
-      runningAvg = runningAvg / listToAvg.size();
-   }
+		runningAvg = runningAvg / listToAvg.size();
+	}
 
-   return runningAvg;
+	return runningAvg;
 }
 
 double FPMath::fpStdDev(std::list<double> &list)
 {
-   double stddev = 0.0;
+	double stddev = 0.0;
 
-   if (list.size() > 0)
-   {
-      double avgOfList = fpAvg(list);
-      double denominator = list.size();
-      double numerator = 0.0;
+	if ( list.size() > 0 )
+	{
+		double avgOfList = fpAvg(list);
+		double denominator = list.size();
+		double numerator = 0.0;
 
-      std::list<double>::iterator iter = list.begin();
-      for (; iter != list.end(); ++iter)
-      {
-         numerator = numerator + pow(*iter - avgOfList, 2);
-      }
+		std::list<double>::iterator iter = list.begin();
+		for ( ; iter != list.end(); ++iter )
+		{
+			numerator = numerator + pow(*iter - avgOfList, 2);
+		}
 
-      // Sample population (not 100%) therefore n-1 for the denominator
-      stddev = sqrt(numerator / (denominator - 1));
-   }
+		// Sample population (not 100%) therefore n-1 for the denominator
+		stddev = sqrt(numerator / (denominator - 1));
+	}
 
-   return stddev;
+	return stddev;
 }
 
 
 bool FPMath::compareEqual(double lhs, double rhs, double epsilon)
 {
-  return (fabs(lhs - rhs) > epsilon) ? false : true;
+	return( fpCompare(lhs,rhs,epsilon) != 0) ? false : true;
 }
 
 bool FPMath::compareEqual(double lhs, double rhs)
 {
-  return compareEqual(lhs, rhs, DBL_EPSILON);
+	return( fpCompare(lhs,rhs) != 0) ? false : true;
 }
 
 bool FPMath::compareGreater(double lhs, double rhs, double epsilon)
 {
-  return (lhs > (rhs + epsilon));
+	return( fpCompare(lhs,rhs,epsilon) > 0) ? true : false;
 }
 
 bool FPMath::compareGreater(double lhs, double rhs)
 {
-  return compareGreater(lhs, rhs, DBL_EPSILON);
+	return( fpCompare(lhs,rhs) > 0) ? true : false;
 }
 
 bool FPMath::compareLess(double lhs, double rhs, double epsilon)
 {
-  return ( lhs < (rhs - epsilon));
+	return( fpCompare(lhs,rhs,epsilon) < 0) ? true : false;
 }
 
 bool FPMath::compareLess(double lhs, double rhs)
 {
-  return FPMath::compareLess(lhs, rhs, DBL_EPSILON);
+	return( fpCompare(lhs,rhs) < 0) ? true : false;
 }
 
 bool FPMath::compareGreaterOrEqual(double lhs, double rhs, double epsilon)
 {
-  return compareEqual(lhs, rhs, epsilon) || compareGreater(lhs, rhs, epsilon);
+	return( fpCompare(lhs,rhs,epsilon) >= 0) ? true : false;
 }
 bool FPMath::compareGreaterOrEqual(double lhs, double rhs)
 {
-  return compareGreaterOrEqual(lhs, rhs, DBL_EPSILON);
+	return( fpCompare(lhs,rhs) >= 0) ? true : false;
 }
 
 bool FPMath::compareLessOrEqual(double lhs,double rhs, double epsilon)
 {
-  return compareEqual(lhs, rhs, epsilon) || compareLess(lhs, rhs, epsilon);
+	return( fpCompare(lhs,rhs,epsilon) <= 0) ? true : false;
 }
 bool FPMath::compareLessOrEqual(double lhs, double rhs)
 {
-  return compareLessOrEqual(lhs, rhs, DBL_EPSILON);
+	return( fpCompare(lhs,rhs) <= 0) ? true : false;
 }
 
 bool FPMath::compareInRange(double value, double min, double max)
 {
-  return compareInRange(value, min, max, DBL_EPSILON);
+	return( fpCompare(value, min) >= 0 && fpCompare(value, max) <= 0 );
 }
 
 bool FPMath::compareInRange(double value, double min, double max, double epsilon)
 {
-  return (compareLessOrEqual(value, max, epsilon) && compareGreaterOrEqual(value, min, epsilon));
+	return( fpCompare(value, min, epsilon) >= 0 && fpCompare(value, max, epsilon) <= 0 );
 }
 
 int FPMath::getNumberOfIntegralDigits(double value)
 {
-  if(FPMath::compareLess(value, 0.0))
-    value = -value;
-  return static_cast<int>(log(value)/log(10.0)) + 1;
+	if ( FPMath::compareLess(value, 0.0) )
+		value = -value;
+	return static_cast<int>(log(value)/log(10.0)) + 1;
 }
 
 bool FPMath::isNan(double value)
 {
-  return value != value;
+	return value != value;
 }
 
 bool FPMath::isInf(double value)
 {
-   if ((value == value) && ((value - value) != 0.0))
-     return true;
-   else
-     return false;
+	if ( (value == value) && ((value - value) != 0.0) )
+		return true;
+	else
+		return false;
 
 }
 
-double FPMath::floor(double value)
+double FPMath::fpFloor(double value)
 {
-  if(compareGreater(value, 0.0))
-    return static_cast<double>(static_cast<int>(value + 0.5));
-  else
-  {
-    double temp = ceiling(value);
-    if(FPMath::compareEqual(temp - value, 0.0))
-      return value;
-    else
-      return static_cast<double>(static_cast<int>(value - 1.0));
-  }
+	if ( compareGreater(value, 0.0) )
+		return static_cast<double>(static_cast<int>(value + 0.5));
+	else
+	{
+		double temp = ceil(value);
+		if ( FPMath::compareEqual(temp - value, 0.0) )
+			return value;
+		else
+			return static_cast<double>(static_cast<int>(value - 1.0));
+	}
 }
 
-double FPMath::ceiling(double value)
+double FPMath::fpCeiling(double value)
 {
-  if(compareGreater(value, 0.0))
-  {
-    double temp = floor(value);
-    if(FPMath::compareEqual(temp - value, 0.0))
-      return value;
-    else
-      return static_cast<double>(static_cast<int>(value + 1.0));
-  }
-  else // Less than 0
-    return static_cast<double>(static_cast<int>(value - 0.5));
+	if ( compareGreater(value, 0.0) )
+	{
+		double temp = floor(value);
+		if ( FPMath::compareEqual(temp - value, 0.0) )
+			return value;
+		else
+			return static_cast<double>(static_cast<int>(value + 1.0));
+	}
+	else // Less than 0
+		return static_cast<double>(static_cast<int>(value - 0.5));
 }
-int FPMath::round(double value)
+
+int FPMath::fpRound(double value)
 {
-  return ( value > 0 ) ? (int)( value + 0.5 ) : (int)( value - 0.5 );
+	return( value > 0 ) ? (int)( value + 0.5 ) : (int)( value - 0.5 );
 }
 

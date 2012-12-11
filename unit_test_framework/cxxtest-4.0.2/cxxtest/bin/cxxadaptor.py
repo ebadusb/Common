2f919e7a16347fbdfe6d360905418f5a88e2f365
Christopher Howells <Christopher.Howells@terumobct.com>
2012-12-11 15:49:01 -0800

    IT 138 - Move all base/safety monitor unit tests into a single VC++ project.
    Jobs: job003016
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 50857]

--- unit_test_framework/cxxtest-4.0.2/cxxtest/bin/cxxadaptor.py
+++ unit_test_framework/cxxtest-4.0.2/cxxtest/bin/cxxadaptor.py
@@ -1,22 +1,45 @@
-# Use a single command line argument (-n INPUT) to generate the following CxxTestGen.py string
-# and execute it as a system command:
+
+# Use command line argument RunnerName with this script to generate the following string and execute it as a system command:
 #
-#     cxxtestgen.py --xunit-printer -w "INPUT" --xunit-file "INPUT Results.xml" -o "INPUT.cpp" "INPUT.h"
+#   python cxxtestgen.py --xunit-printer -w RunnerName --xunit-file="RunnerName Results.xml" -o RunnerName.cpp RunnerName.h
+#   (This will generate a test runner from a single header file with the same name)
+#
+# Include optional argument [--headers] to generate the following string and execute it as a system command:
+#
+#   python cxxtestgen.py --xunit-printer -w RunnerName --xunit-file="RunnerName Results.xml" -o RunnerName.cpp --headers=RunnerName.lst
+#   (This will generate a test runner from potentially multiple header files included in a .lst file with the same name)
 
-import sys
-import os
-import subprocess
-from optparse import OptionParser
 
+if __name__ == "__main__":
 
+    import sys
+    import os
+    import subprocess
+    from optparse import OptionParser
 
-parser = OptionParser("%prog [options] [<filename> ...]")
-parser.add_option("-n", "--name",
-                  action="store", type="string", dest="name",
-                  help="Name of VC++ Project")
-options, args = parser.parse_args(sys.argv)
-cxxtestgen = "python "+os.environ["CXXTEST"] +"\\bin\\cxxtestgen.py --xunit-printer -w \"<INPUT>\" --xunit-file \"<INPUT> Results.xml\" -o \"<INPUT>.cpp\" \"<INPUT>.h\""
-cxxtestgen = cxxtestgen.replace("<INPUT>", options.name);
-output = subprocess.Popen(cxxtestgen, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
-for line in output: print(line)
-   
+    parser = OptionParser("%prog [options] RunnerName")
+    parser.add_option("--headers", action="store_true", dest="headers", default=False, help="File name of listed header files")
+    options, args = parser.parse_args()
+
+    cxxtestgen = ""
+    if args:
+        name = args[0]
+
+        if options.headers and os.path.isfile(name + ".lst"):
+            cxxtestgen = "python " + os.path.join(os.environ["CXXTEST"], "bin\\cxxtestgen.py") + " --xunit-printer -w \"" + \
+                           name + "\" --xunit-file=" + "\"" + name + " Results.xml\" -o \"" + name + ".cpp\" " + \
+                            "--headers=\"" + name + ".lst\""
+
+        elif os.path.isfile(name + ".h"):
+            cxxtestgen = "python " + os.path.join(os.environ["CXXTEST"], "bin\\cxxtestgen.py") + " --xunit-printer -w \"" + \
+                           name + "\" --xunit-file=" + "\"" + name + " Results.xml\" -o \"" + name + ".cpp\" " + \
+                            "\"" + name + ".h\""
+
+    if cxxtestgen:
+
+        print cxxtestgen
+        output = subprocess.Popen(cxxtestgen, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
+        for line in output: print(line)
+
+    else:
+        parser.print_help()

098a9d9993f9831ac162a1864a94f5cb577f408f
cmueusb <a@b>
2014-04-30 10:18:25 -0800

    - Taos IT#13879/13793/13797/13795/13792/13801/3065
    (13879) - Implement new screen hiearchy
    (13879)  - Move Menu Buttons from the BaseScreen to the TabManager
    (13879)  - Update ScreenManager to use a list of screens sorted in z order. Also each element should contain a screen type, z order, and screen pointer
    (13879)  - change tabs disabled to a instance variable and update TabManager interface names to makeTabsAvailable / makeTabsUnavailable
    
    (13793) - Update the YesNoButton class to be consistent with the style guide ( highlight changed values and set disabled text gray )
    
    (13797) -  Create a new menu button class consistent with the style guide
    
    (13795) - Update the DataEntryIconButton to implement label and units style guide requirements
    
    (13792) - Create a new button base class to ensure buttons text colors follow the style guide
    
    (13801) - Update base layer screens to use BaseButton instead of CGUIButton to Fix white disabled text issues 
    (13801) - Create new MuteButton base class
    (13801) - Create new ListScrollButton base class
    (13801) - Create new Procedure / File selection button base class
    (13801) - Create New Patient Data Button Base class
    (13801) - Update CauseButton , Continue, GoBack button styles for consistency with the style guide
    
    (3065) - Create new Configuration Buttons which set Yellow Text when values change
    (3065) -Create NumericReadout class for uneditable readouts on run values, main run, and run targets screens
    
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 59006]

--- cgui/unit_test/test_screen.cpp
+++ cgui/unit_test/test_screen.cpp
@@ -1,7 +1,7 @@
 /*
- * $Header: 
+ * $Header:
  *
- * $Log:  
+ * $Log:
  *
  */
 
@@ -12,113 +12,118 @@ UnicodeString testUstr;
 bool useDefault=true;
 int BoxHeight=100;
 int BoxWidth=100;
-int BoxAttrib=0; 
+int BoxAttrib=0;
 int BoxX=100;
 int BoxY=100;
 
 void setTestText(char* str)
 {
-	testUstr = str;
-	useDefault = false;
+   testUstr = str;
+   useDefault = false;
 }
 
 TestScreen::TestScreen( CGUIDisplay & display,
-												const CallbackBase goBackCallback):
-	BaseScreen(display),
-	_goBackCallback(goBackCallback)
+                        const CallbackBase goBackCallback):
+BaseScreen(display),
+   _goBackCallback(goBackCallback)
 {
-	_backgroundBmp = NULL;
-	_cguiBitmapInfo = NULL;
+   _backgroundBmp = NULL;
+   _cguiBitmapInfo = NULL;
 
-	unsigned long height = BaseReportConfig::ScreenHeight;
-	unsigned long width = BaseReportConfig::ScreenWidth;
+   unsigned long height = BaseReportConfig::ScreenHeight;
+   unsigned long width = BaseReportConfig::ScreenWidth;
 
-	for( int i= 0 ; i < height ; i++ )
-	{
-		for( int j=0 ; j < width ; j++ )
-		{
-			if( ( i == BoxY-1 && ( j >= BoxX-1 && j <= (BoxX+BoxWidth) ) ) ||
-				 ( i == (BoxY+BoxHeight) && ( j >= BoxX-1 && j <= (BoxX+BoxWidth) ) ) ||
-				 ( j == BoxX-1 && ( i >= BoxY-1 && i <= (BoxY+BoxHeight) ) ) || 
-				 ( j == (BoxX+BoxWidth) && ( i >= BoxY-1 && i <= (BoxY+BoxHeight) ) ) )
-			{
-				_bmp[i][j] = grayToRGB565(0x00);
-			}
-			else
-			{
-				_bmp[i][j] = grayToRGB565(0xFF);
-			}
-		}
-	} 
+   for ( int i= 0 ; i < height ; i++ )
+   {
+      for ( int j=0 ; j < width ; j++ )
+      {
+         if ( ( i == BoxY-1 && ( j >= BoxX-1 && j <= (BoxX+BoxWidth) ) ) ||
+              ( i == (BoxY+BoxHeight) && ( j >= BoxX-1 && j <= (BoxX+BoxWidth) ) ) ||
+              ( j == BoxX-1 && ( i >= BoxY-1 && i <= (BoxY+BoxHeight) ) ) ||
+              ( j == (BoxX+BoxWidth) && ( i >= BoxY-1 && i <= (BoxY+BoxHeight) ) ) )
+         {
+            _bmp[i][j] = grayToRGB565(0x00);
+         }
+         else
+         {
+            _bmp[i][j] = grayToRGB565(0xFF);
+         }
+      }
+   }
 
-	_cguiBitmapInfo = new CGUIBitmapInfo(_bmp[0],
-													 (unsigned long) height*width,
-													 (unsigned short) width,
-													 (unsigned short) height);
+   _cguiBitmapInfo = new CGUIBitmapInfo(_bmp[0],
+                                        (unsigned long) height*width,
+                                        (unsigned short) width,
+                                        (unsigned short) height);
 
-	if( _backgroundBmp == NULL )
-		_backgroundBmp = new CGUIBitmap (_display, CGUIRegion(0,0,0,0), *_cguiBitmapInfo );
+   if ( _backgroundBmp == NULL )
+      _backgroundBmp = new CGUIBitmap (_display, CGUIRegion(0,0,0,0), *_cguiBitmapInfo );
 }
 
 TestScreen::~TestScreen()
 {
-	if ( _goBackButton )
-	{
-		delete _goBackButton;
-		_goBackButton = NULL;
-	}
+   if ( _goBackButton )
+   {
+      delete _goBackButton;
+      _goBackButton = NULL;
+   }
 
-	if( _printNotesSummaryText )
-	{
-		delete _printNotesSummaryText;
-		_printNotesSummaryText =  NULL;
-	}
+   if ( _printNotesSummaryText )
+   {
+      delete _printNotesSummaryText;
+      _printNotesSummaryText =  NULL;
+   }
 
-	if( _cguiBitmapInfo != NULL )
-	{
-		delete _cguiBitmapInfo;
-		_cguiBitmapInfo=NULL;
-	}
+   if ( _cguiBitmapInfo != NULL )
+   {
+      delete _cguiBitmapInfo;
+      _cguiBitmapInfo=NULL;
+   }
 
-	if ( _backgroundBmp != NULL )
-	{
-		delete _backgroundBmp;
-		_backgroundBmp = NULL;
-	}
+   if ( _backgroundBmp != NULL )
+   {
+      delete _backgroundBmp;
+      _backgroundBmp = NULL;
+   }
 }
 
 unsigned short TestScreen::grayToRGB565(unsigned char pixelValue)
 {
-	unsigned short pixel = ( unsigned short) pixelValue;
+   unsigned short pixel = ( unsigned short) pixelValue;
 
-	unsigned short red = pixel >> 3 ;   // first 5 MSB's 
-	unsigned short green = pixel >> 2;  // mid 6 bits
-	unsigned short blue = pixel >> 3;   // 5 LSB's
+   unsigned short red = pixel >> 3 ;   // first 5 MSB's
+   unsigned short green = pixel >> 2;  // mid 6 bits
+   unsigned short blue = pixel >> 3;   // 5 LSB's
 
-	red = red << 11;
-	green = green << 5;
+   red = red << 11;
+   green = green << 5;
 
-	unsigned short rgb565Pixel = 0;
-	rgb565Pixel = rgb565Pixel | red | green | blue;
-	return rgb565Pixel;
+   unsigned short rgb565Pixel = 0;
+   rgb565Pixel = rgb565Pixel | red | green | blue;
+   return rgb565Pixel;
 }
 
 void TestScreen::setModeTabState(bool state)
 {
-	BaseScreen::_configModeButton->setWindowVisibility(state);
-	BaseScreen::_dataModeButton->setWindowVisibility(state);
-	BaseScreen::_runModeButton->setWindowVisibility(state);
-	BaseScreen::_endRunModeButton->setWindowVisibility(state);
-	BaseScreen::setTempTabsDisabled(!state);
-	refreshModeButtons();
+   if ( state )
+   {
+      TabManager::instance().showMenuButtons( this );
+      makeTabsAvailable();
+   }
+   else
+   {
+      TabManager::instance().hideMenuButtons();
+      makeTabsUnavailable();
+   }
+   refreshModeButtons();
 }
 
 void TestScreen::disableAlarmActiveButton()
 {
-	if( _activeAlarmButton != NULL && _activeAlarmButton->isVisible() )
-	{
-		_activeAlarmButton->invisible();
-	}
+   if ( _activeAlarmButton != NULL && _activeAlarmButton->isVisible() )
+   {
+      _activeAlarmButton->invisible();
+   }
 }
 
 // Notice start
@@ -145,118 +150,115 @@ void TestScreen::addAdornmentTextObject()
 
 void TestScreen::initializeTestScreen()
 {
-	initializeScreen(NULL);
+   initializeScreen(NULL);
 
-	if(_screenshotButton)
-	{
-		disableSnapButton();				 // Screen Shot stuff
-		_screenshotButton->detach();
-	}
+   if ( _screenshotButton )
+   {
+      disableSnapButton();                             // Screen Shot stuff
+      _screenshotButton->detach();
+   }
 
-	BaseScreen::setTempTabsDisabled(true);
-	BaseScreen::_configModeButton->setWindowVisibility(false);
-	BaseScreen::_dataModeButton->setWindowVisibility(false);
-	BaseScreen::_runModeButton->setWindowVisibility(false);
-	BaseScreen::_endRunModeButton->setWindowVisibility(false);
+   makeTabsUnavailable();
+   TabManager::instance().hideMenuButtons();
 
-	_goBackButtonData.left = 270;
-	_goBackButtonData.top = 40;
-	_goBackButtonData.vMargin = 3;
-	_goBackButtonData.hMargin = 3;
-	_goBackButtonData.enabledTextItem = NULL;
-	_goBackButtonData.enabledStylingRecord = NULL;
+   _goBackButtonData.left = 270;
+   _goBackButtonData.top = 40;
+   _goBackButtonData.vMargin = 3;
+   _goBackButtonData.hMargin = 3;
+   _goBackButtonData.enabledTextItem = NULL;
+   _goBackButtonData.enabledStylingRecord = NULL;
 
-	_goBackButtonData.disabledBitmapId = BaseBitmaps::gobackDisabledButton;
-	_goBackButtonData.disabledTextItem = NULL;
-	_goBackButtonData.disabledStylingRecord = NULL;
+   _goBackButtonData.disabledBitmapId = BaseBitmaps::gobackDisabledButton;
+   _goBackButtonData.disabledTextItem = NULL;
+   _goBackButtonData.disabledStylingRecord = NULL;
 
-	_goBackButtonData.pressedTextItem = NULL;
-	_goBackButtonData.pressedStylingRecord = NULL;
+   _goBackButtonData.pressedTextItem = NULL;
+   _goBackButtonData.pressedStylingRecord = NULL;
 
-	strcpy(_goBackButtonData.alternateButtonId, "GoBackReport");
-	_goBackButtonData.type = CGUIButton::RaiseAfterRelease;
+   strcpy(_goBackButtonData.alternateButtonId, "GoBackReport");
+   _goBackButtonData.type = CGUIButton::RaiseAfterRelease;
 
    bool ifScreenDefault = false;
 
-	switch ( BaseScreen::getScreenState() )
-	{
-	case GuiCommonDef::ScreenDefault:
-		DataLog(log_level_gui_info) << " ERROR : GUI Commondef state is ScreenDefault  " << endmsg;
-		ifScreenDefault = true;
+   switch ( getScreenState() )
+   {
+      case GuiCommonDef::ScreenDefault:
+         DataLog(log_level_gui_info) << " ERROR : GUI Commondef state is ScreenDefault  " << endmsg;
+         ifScreenDefault = true;
 
-	case GuiCommonDef::Alarm:
-		if( ifScreenDefault == false )
-			DataLog(log_level_gui_info) << " ERROR : GUI Commondef state is Alarm  " << endmsg;
+      case GuiCommonDef::Alarm:
+         if ( ifScreenDefault == false )
+            DataLog(log_level_gui_info) << " ERROR : GUI Commondef state is Alarm  " << endmsg;
 
-	case GuiCommonDef::PreRun:
-		_goBackButtonData.enabledBitmapId = BaseBitmaps::dismissEnabledPrerunButton;
-		_goBackButtonData.pressedBitmapId = BaseBitmaps::dismissPressedPrerunButton;
-		break;
+      case GuiCommonDef::PreRun:
+         _goBackButtonData.enabledBitmapId = BaseBitmaps::dismissEnabledPrerunButton;
+         _goBackButtonData.pressedBitmapId = BaseBitmaps::dismissPressedPrerunButton;
+         break;
 
-	case GuiCommonDef::PostRun:
+      case GuiCommonDef::PostRun:
 
-		_goBackButtonData.enabledBitmapId = BaseBitmaps::dismissEnabledPostrunButton;
-		_goBackButtonData.pressedBitmapId = BaseBitmaps::dismissPressedPostrunButton;
-		break;
+         _goBackButtonData.enabledBitmapId = BaseBitmaps::dismissEnabledPostrunButton;
+         _goBackButtonData.pressedBitmapId = BaseBitmaps::dismissPressedPostrunButton;
+         break;
 
-	case GuiCommonDef::Run:
-		_FATAL_ERROR(__FILE__, __LINE__, "Attempted to create a report screen during a run ");
-		break;
+      case GuiCommonDef::Run:
+         _FATAL_ERROR(__FILE__, __LINE__, "Attempted to create a report screen during a run ");
+         break;
 
-	}
+   }
 
-	CGUITextItem notesCopy(BaseStringData::printNotesSummary);
+   CGUITextItem notesCopy(BaseStringData::printNotesSummary);
 
-	StylingRecord sr = notesCopy.getStylingRecord();
+   StylingRecord sr = notesCopy.getStylingRecord();
 
-	sr.region.x = BoxX;
-	sr.region.y = BoxY;
-	sr.region.width = BoxWidth;
-	sr.region.height = BoxHeight;
-	sr.attributes = BoxAttrib;
+   sr.region.x = BoxX;
+   sr.region.y = BoxY;
+   sr.region.width = BoxWidth;
+   sr.region.height = BoxHeight;
+   sr.attributes = BoxAttrib;
 
-	notesCopy.setStylingRecord(sr);
+   notesCopy.setStylingRecord(sr);
 
-	if( useDefault )
-	{
-		UnicodeString ustr = BaseStringData::testJapString.getTextObj();
-		testUstr = ustr;
-	}
-		
-	notesCopy.setText(testUstr.getString());
+   if ( useDefault )
+   {
+      UnicodeString ustr = BaseStringData::testJapString.getTextObj();
+      testUstr = ustr;
+   }
 
-	_printNotesSummaryText = new CGUIText(_display,&notesCopy);
+   notesCopy.setText(testUstr.getString());
 
-	addObjectToFront(_backgroundBmp);
-	addObjectToFront(_printNotesSummaryText);
+   _printNotesSummaryText = new CGUIText(_display,&notesCopy);
+
+   addObjectToFront(_backgroundBmp);
+   addObjectToFront(_printNotesSummaryText);
 }
 
 void TestScreen::disableButtons()
 {
-	if ( _goBackButton )
-	{
-		_goBackButton->disable();
-	}
+   if ( _goBackButton )
+   {
+      _goBackButton->disable();
+   }
 }
 
 void TestScreen::hideButtons(void)
 {
-	if ( _goBackButton )
-	{
-		_goBackButton->invisible();
-	}
+   if ( _goBackButton )
+   {
+      _goBackButton->invisible();
+   }
 }
 
 void TestScreen::createButtons()
 {
-	_goBackButton = new CGUIButton(_display, this, _goBackButtonData);
-	_goBackButton->setReleasedCallback(_goBackCallback);
-	_goBackButton->disable();
+   _goBackButton = new CGUIButton(_display, this, _goBackButtonData);
+   _goBackButton->setReleasedCallback(_goBackCallback);
+   _goBackButton->disable();
 }
 
 void TestScreen::enableButtons(short pageNumber,short noOfPages)
 {
-	_goBackButton->enable();
+   _goBackButton->enable();
 }
 
 

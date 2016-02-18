126918e61ad843bea3ab11f2134e9aeaad24b9ad
bmetusb <a@b>
2016-02-17 16:51:45 -0800

    Add APC pxe bootrom build (EBox 2016)
    [git-p4: depot-paths = "//depot/main/embedded/Common/": change = 70942]

--- vxworks/bootrom/safety/bootline.h
+++ vxworks/bootrom/safety/bootline.h
@@ -10,10 +10,26 @@
 #define INTFTPPASS "Aunt_078"
 
 #define DEFAULT_BOOT_LINE \
-  "(0,0)control:/taos/boot/safety/vxWorks" \
+  "gei(0,0)control:/taos/boot/safety/vxWorks" \
   " e=90.1.0.1:ffffff00 h=90.1.0.2" \
   " u=" INTFTPUSER " pw=" INTFTPPASS \
-  " f=0"
+  " f=0x80"
+
+#define BOOTLOAD_TAG   "TAOS"
+
+#endif /* OPTIA_BOOTROM */
+/* ================================================================================ */
+
+#ifdef OPTIA_APC_BOOTROM
+
+#define INTFTPUSER "david"
+#define INTFTPPASS "Aunt_078"
+
+#define DEFAULT_BOOT_LINE \
+  "gei(0,0)control:/taos/boot/apc/vxWorks" \
+  " e=90.2.0.1:ffffff00 h=90.2.0.2" \
+  " u=" INTFTPUSER " pw=" INTFTPPASS \
+  " f=0x80"
 
 #define BOOTLOAD_TAG   "TAOS"
 
